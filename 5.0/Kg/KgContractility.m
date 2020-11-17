2d6d9160027c4e90c9a262f144019ef2c60222af
Pablo Vicente Munuera <pablovm1990@gmail.com>
2020-11-17 19:06:56 +0000

    Init contractility

---
+++ 5.0/Kg/KgContractility.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = KgContractility(inputArg1,inputArg2)
+%KGCONTRACTILITY Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
---
+++ 5.0/Kg/gContractility.m
@@ -0,0 +1,6 @@
+function [] = gContractility(Cell,Y,Set)
+%GCONTRACTILITY Summary of this function goes here
+%   Detailed explanation goes here
+
+end
+

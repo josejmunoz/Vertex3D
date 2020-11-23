7d6f0c5ae1edc87ca2e19db1b644a9afe52dc5f6
Pablo Vicente Munuera <pablovm1990@gmail.com>
2020-11-23 14:56:06 +0000

    Update gContractility.m
    Starting to put basics

--- 5.0/Kg/gContractility.m
+++ 5.0/Kg/gContractility.m
@@ -2,5 +2,25 @@ function [] = gContractility(Cell,Y,Set)
 %GCONTRACTILITY Summary of this function goes here
 %   Detailed explanation goes here
 
+%% Set parameters
+currentCell=Cell.n;
+
+%% Initialize
+dimensionsG = %Set.NumTotalV*3;
+
+g = zeros(dimensionsG,1); % Local cell residual
+
+EnergyContractility=0;
+
+C=Set.cContractility;
+
 end
 
+
+function [gContractility] = computeGContractility(C)
+%COMPUTEGCONTRACTILITY Summary of this function goes here
+%   Detailed explanation goes here
+
+
+
+end

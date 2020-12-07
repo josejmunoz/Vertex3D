898cc5f4de5f7d087e7f9cb6ae0b483fb5e068cf
Pablo Vicente Munuera <pablovm1990@gmail.com>
2020-12-07 10:04:54 +0000

    Update KgContractility.m
    Changes after talking to J.J. Munoz

--- 5.0/Kg/KgContractility.m
+++ 5.0/Kg/KgContractility.m
@@ -16,6 +16,29 @@ EnergyContractility = 0;
 
 C = Set.cContractility;
 
+Energy = 0;
+
+[g] = computeGContractility(Cell, C);
+
 
 end
 
+function [kgContractility] = computeKGContractility(Cell, C)
+%COMPUTEGCONTRACTILITY Summary of this function goes here
+%   Detailed explanation goes here
+
+[Cell] = Cell.computeEdgeLengths();
+Cell.EdgeLengths = Cell.EdgeLengths;
+
+end
+
+function [gContractility] = computeGContractility(Cell, C)
+%COMPUTEGCONTRACTILITY Summary of this function goes here
+%   Detailed explanation goes here
+
+[Cell] = Cell.computeEdgeLengths();
+Cell.EdgeLengths = Cell.EdgeLengths;
+
+gContractility = 0.5 * (edgeLength - (edgeLength0 * C))^2;
+
+end

12bb03b0d5510d46c88ac227b9ad1fbbde767f40
MALIK-DAWI <33166797+MALIK-DAWI@users.noreply.github.com>
2020-12-11 16:43:23 +0100

    Update remodeling files

--- 5.0/Remodel/SolveRemodelingStep.m
+++ 5.0/Remodel/SolveRemodelingStep.m
@@ -1,237 +1,193 @@
-function [Cell,Faces,Y,Yn,SCn,X,Dofs,Set,Energy,DidNotConverge]=SolveRemodelingStep(Cell,Faces,Y,X,Dofs,Set,Yn,SCn)
+function [Cell,Faces,Y,Yn,SCn,X,Dofs,Set,Energy,DidNotConverge]=SolveRemodelingStep(Cell,Faces,Y,X,Dofs,Set,Yn,SCn,CellInput,XgSub)
+
+% This function solves local problem to obtain the position of the newly
+% remodeled vertices with prescribed settings (Set.***_LP), e.g.
+% Set.lambda_LP. 
+
+% Remark: The convergence strategy (regularization with viscosity) is 
+% initiated from the first iteration by setting (Set.nu_LP_Inital>Set.nu) 
+% and then it is reduced progressively. The solution is considered to be 
+% converged only when the prescribed value of global viscosity (Set.nu) is reached.
+
+
 
 
 Cell.AssembleAll=false;
-
-
-fprintf('=====>> Remodeling Step.\n');
+fprintf('=====>> Solving Local Problem....\n');
 Yp=Y;
 Cellp=Cell;
 Ynp=Yn;
 SCnp=SCn;
 IncreaseEta=true;
-InitVtk('CellShapiter')
-PostProcessingV2(X,Y.DataOrdered,[],[],Cell,'CellShapiter',0,[])
 
-Set.lambdaV0=Set.lambdaV;
+
+% Copy Global Settings 
+AuxlambdaV=Set.lambdaV;
+AuxEnergyBarrier=Set.EnergyBarrier;
+AuxlambdaB=Set.lambdaB;
+AuxBeta=Set.Beta;
+AuxBending=Set.Bending;
+AuxBendingAreaDependent=Set.BendingAreaDependent;
+AuxlambdaBend=Set.lambdaBend;
+Auxnu=Set.nu;
+
+
+
+
+Set.lambdaV=Set.lambdaV_LP;
+Set.EnergyBarrier=Set.EnergyBarrier_LP;
+Set.lambdaB=Set.lambdaB_LP;
+Set.Beta=Set.Beta_LP;
+Set.Bending=Set.Bending_LP;
+Set.BendingAreaDependent=Set.BendingAreaDependent_LP;
+Set.lambdaBend=Set.lambdaBend_LP;
+
+
+
 Set.nu0=Set.nu;
-
-Set.Bending0=Set.Bending;
-Set.lambdaBend0=Set.lambdaBend;
-Set.BendingAreaDependent0=Set.BendingAreaDependent;
+Set.nu=Set.nu_LP_Inital;
 
 
 
-Set.lambdaV=0.5;
-Set.nu=1;
-
-Set.Bending=true;
-Set.lambdaBend=.01;
-Set.BendingAreaDependent0=false;
-
-Set.MaxIter=Set.MaxIter0*2;
 
 
-Incr=0;
+
+
+
+% Set.lambdaV0=Set.lambdaV;
+% Set.nu0=Set.nu;
+% 
+% Set.Bending0=Set.Bending;
+% Set.lambdaBend0=Set.lambdaBend;
+% Set.BendingAreaDependent0=Set.BendingAreaDependent;
+% 
+% 
+% % Set.lambdaV=0.5;
+% Set.nu=1;
+% 
+% Set.Bending=false;
+% Set.lambdaBend=.05;
+% Set.BendingAreaDependent=false;
+
+
+
+Set.MaxIter=Set.MaxIter0/2;
 
 while(1)
-    if Set.Parallel   
-        [gs,Ks,Cell,Es]=KgSurface(Cell,Y,Faces,Set);
-        [gv,Kv,Cell,Ev]=KgVolumeParallel(Cell,Y,Set);
-        [gb,Kb,Cell,Eb]=KgBendingParallel(Cell,Y,Set);
-    else
-        [gs,Ks,Cell,Es]=KgSurface(Cell,Y,Faces,Set);
-        [gv,Kv,Cell,Ev]=KgVolume(Cell,Y,Set);       
-        [gb,Kb,Cell,Eb]=KgBending(Cell,Y,Set);
-    end 
-    if Set.EnergyBarrier && Set.Parallel
-        [gB,KB,Cell,EB]=KgTriEnergyBarrierParallel(Cell,Y,Set);
-    elseif Set.EnergyBarrier
-        [gB,KB,Cell,EB]=KgTriEnergyBarrier(Cell,Y,Set);
-    else 
-        gB=zeros(size(gv));
-        KB=zeros(size(Kv));
-        EB=0;
-    end
-    if Set.Contractility
-        [gc,Kc,Cell,Ec]=KgContractility(Cell,Y,Set);
-    else
-        gc = zeros(size(gv));
-        Kc = zeros(size(Kv));
-        Ec = 0;
-    end
-    
     Ytn=[Yn.DataOrdered ;SCn.DataOrdered];
-    Yt=[Y.DataOrdered ;Cell.SurfsCenters.DataOrdered];
-
+    Yt=[Y.DataOrdered ;Cell.FaceCentres.DataOrdered];
     y=reshape(Yt',Set.NumTotalV*3,1);
     yn=reshape(Ytn',Set.NumTotalV*3,1);
 
-    %damping
-    Kf=(Set.nu/Set.dt).*sparse(eye(size(Kv)));
-    gf=(Set.nu/Set.dt).*(y-yn);
-
-
-
-    K=Kv+Kf+Ks+KB+Kb+Kc;
-    g=gv+gf+gs+gB+gb+gc;
+    [g,K,Cell,Energy]=KgGlobal(Cell,Faces,Y,y,yn,Set,CellInput,XgSub);
+    
     dy=zeros(size(y));
-
     dyr=norm(dy(Dofs.Remodel));
     gr=norm(g(Dofs.Remodel));   
-    fprintf('Iter: %i, ||gr||= %e ||dyr||= %e \n',0,gr,dyr);
+    gr0=gr;
+    fprintf('Local Problem ->Iter: %i, ||gr||= %.3e ||dyr||= %.3e  nu/nu0=%.3e  dt/dt0=%.3g \n',0,gr,dyr,Set.nu/Set.nu0,Set.dt/Set.dt0);
 
-     gggr=zeros(3,1);
-     gggr(1)=gr;
-     ig=1;
-     Set.iter=1;
-
+    
+    gggr=zeros(3,1);
+    gggr(1)=gr;
+    ig=1;
+    Set.iter=1;
+    
     while (gr>Set.tol || dyr>Set.tol) && Set.iter<Set.MaxIter
-
+        
         dy(Dofs.Remodel)=-K(Dofs.Remodel,Dofs.Remodel)\g(Dofs.Remodel);
-        [alpha0]=LineSearch(Cell,Faces,y,yn,dy,g,Dofs.Remodel,Set,Y);  
-
-        y=y+alpha0*dy; % update nodes
+        [alpha0]=LineSearch(Cell,Faces,y,yn,dy,g,Dofs.Remodel,Set,Y,CellInput,XgSub);
+        
+        y=y+alpha0*dy; % update 
         Yt=reshape(y,3,Set.NumTotalV)';
         Y=Y.Modify(Yt(1:Set.NumMainV,:));
-        Cell.SurfsCenters=Cell.SurfsCenters.Modify(Yt(Set.NumMainV+1:Set.NumTotalV,:));
-        if Set.Parallel   
-            [gs,Ks,Cell,Es]=KgSurface(Cell,Y,Faces,Set);
-            [gv,Kv,Cell,Ev]=KgVolumeParallel(Cell,Y,Set);
-            [gb,Kb,Cell,Eb]=KgBendingParallel(Cell,Y,Set);
-        else
-            [gs,Ks,Cell,Es]=KgSurface(Cell,Y,Faces,Set);
-            [gv,Kv,Cell,Ev]=KgVolume(Cell,Y,Set);       
-            [gb,Kb,Cell,Eb]=KgBending(Cell,Y,Set);
-        end 
-        if Set.EnergyBarrier && Set.Parallel
-            [gB,KB,Cell,EB]=KgTriEnergyBarrierParallel(Cell,Y,Set);
-        elseif Set.EnergyBarrier
-            [gB,KB,Cell,EB]=KgTriEnergyBarrier(Cell,Y,Set);
-        else 
-            gB=zeros(size(gv));
-            KB=zeros(size(Kv));
-            EB=0;
-        end
-        if Set.Contractility
-            [gc,Kc,Cell,Ec]=KgContractility(Cell,Y,Set);
-        else
-            gc = zeros(size(gv));
-            Kc = zeros(size(Kv));
-            Ec = 0;
-        end
-
+        Cell.FaceCentres=Cell.FaceCentres.Modify(Yt(Set.NumMainV+1:Set.NumTotalV,:));
         
-        if Set.nu > Set.nu0 &&  gr<1e-8
+        % Reduced viscosity
+        if Set.nu > Set.nu0 &&  gr<1e-6
             Set.nu = max(Set.nu/2,Set.nu0);
-%         elseif Set.nu <= Set.nu0 && gr <1e-8 && Incr<=5
-%             yn(Dofs.Remodel)=y(Dofs.Remodel); 
-%             Incr=Incr+1;
-%             Set.MaxIter=Set.MaxIter0*10;
         end
         
+        [g,K,Cell,Energy]=KgGlobal(Cell,Faces,Y,y,yn,Set,CellInput,XgSub);        
         
-
-        %damping
-        Kf=(Set.nu/Set.dt).*sparse(eye(size(Kv)));
-        gf=(Set.nu/Set.dt).*(y-yn);
-        K=Kv+Kf+Ks+KB+Kb+Kc;
-        g=gv+gf+gs+gB+gb+gc;
         dyr=norm(dy(Dofs.Remodel));
         gr=norm(g(Dofs.Remodel));
-
-        fprintf('Iter: %i,  ||gr||= %e ||dyr||= %e alpha= %d \n',Set.iter,gr,dyr,alpha0);
-%          PostProcessingV2(X,Y.DataOrdered,[],[],Cell,'CellShapiter',Set.iter,[])
-
+        
+        fprintf('Local Problem ->Iter: %i,  ||gr||= %.3e ||dyr||= %.3e alpha= %.3e nu/nu0=%.3g \n',Set.iter,gr,dyr,alpha0,Set.nu/Set.nu0);
+        
         Set.iter=Set.iter+1;
-
+        Set.N_Remodeling_Iterations=Set.N_Remodeling_Iterations+1;
+        
         gggr(ig+1)=gr;
         if ig ==2
             ig=0;
-        else 
+        else
             ig=ig+1;
-        end 
-        if abs(gggr(1)-gggr(2))/gggr(1)<1e-4 &&...
-           abs(gggr(1)-gggr(3))/gggr(1)<1e-4 &&...
-           abs(gggr(3)-gggr(2))/gggr(3)<1e-4 
-           Set.iter=Set.MaxIter;
-        end 
-    end 
+        end
+         if (abs(gggr(1)-gggr(2))/gggr(1)<1e-3 &&...
+             abs(gggr(1)-gggr(3))/gggr(1)<1e-3 &&...
+             abs(gggr(3)-gggr(2))/gggr(3)<1e-3) || ... 
+             abs((gr0-gr)./gr0)>1e3
+             Set.iter=Set.MaxIter;
+         end
+    end
     
     if IncreaseEta &&  (gr>Set.tol || dyr>Set.tol)
-%         %  repeat the step increase eta 
+        fprintf('Convergence was not achieved ... \n');
+        fprintf('First strategy ---> Restart iterating while higher viscosity... \n');
         Y=Yp;
         Cell=Cellp;
         Yn=Ynp;
         SCn=SCnp;
-        Set.nu=Set.nu0*10;
+        Set.nu=Set.nu*10;
         Set.MaxIter=Set.MaxIter0*4;
-        fprintf('===>>  Repeat the step while increase eta.\n');
         IncreaseEta=false;
         
 %     elseif Set.iter == Set.MaxIter && Set.iter > Set.MaxIter0  && Set.dt>Set.dt0/2 && (gr>Set.tol || dyr>Set.tol)
-%         % step halfing 
+%          fprintf('Convergence was not achieved ... \n');
+%          fprintf('Second strategy ---> Restart iterating with half step-size...\n');
 %         Y=Yp;
 %         Cell=Cellp;
 %         Set.MaxIter=Set.MaxIter0;
 %         Set.nu=Set.nu0;
 %         Set.dt=Set.dt/2;
-%         fprintf('=====>>  Do Step-Halfing.\n');
         
     elseif gr>Set.tol || dyr>Set.tol || any(isnan(g(Dofs.FreeDofs))) || any(isnan(dy(Dofs.FreeDofs)))
         % this should not take a place
-        fprintf('Remodeling-step did not converge after %i iterations.\n',Set.iter);
+        fprintf('Local Problem did not converge after %i iterations.\n',Set.iter);
         Set.MaxIter=Set.MaxIter0;
         DidNotConverge=true;
-        Energy.Ev=Ev;
-        Energy.Es=Es;
-        Energy.Ef=(1/2)*(gf')*gf/Set.nu;
-        Energy.EB=EB;
-        
-        Energy.Eb=Eb; % This was not here, why???
-        
-        Energy.Ec=Ec;
 
-        Set.lambdaV=Set.lambdaV0;
-        Set.nu=Set.nu0;
-        
-        Set.Bending=Set.Bending0;
-        Set.lambdaBend=Set.lambdaBend0;
-        Set.BendingAreaDependent=Set.BendingAreaDependent0;
-        
+
+        Set.lambdaV=AuxlambdaV;
+        Set.EnergyBarrier=AuxEnergyBarrier;
+        Set.lambdaB=AuxlambdaB;
+        Set.Beta=AuxBeta;
+        Set.Bending=AuxBending;
+        Set.BendingAreaDependent=AuxBendingAreaDependent;
+        Set.lambdaBend=AuxlambdaBend;
+        Set.nu=Auxnu;
         break;
+        
     else 
-%       [X]=updateX(X,Y,Ytn,T,Set);
         Ytn=reshape(yn,3,Set.NumTotalV)';
         Yn=Yn.Modify(Ytn(1:Set.NumMainV,:));
         SCn=SCn.Modify(Ytn(Set.NumMainV+1:Set.NumTotalV,:));
-        Energy.Ev=Ev;
-        Energy.Es=Es;
-        Energy.Ef=(1/2)*(gf')*gf/Set.nu;
-        Energy.EB=EB;
-        Energy.Eb=Eb;
         Set.MaxIter=Set.MaxIter0;
-
-        fprintf('=====>> Remodeling-step converged in %i iterations.\n',Set.iter);
+        fprintf('=====>> Local Problem converged in %i iterations.\n',Set.iter);
         DidNotConverge=false;
         
-        Set.lambdaV=Set.lambdaV0;
-        Set.nu=Set.nu0;
-        
-        Set.Bending=Set.Bending0;
-        Set.lambdaBend=Set.lambdaBend0;
-        Set.BendingAreaDependent=Set.BendingAreaDependent0;
-        
-        
-
+        Set.lambdaV=AuxlambdaV;
+        Set.EnergyBarrier=AuxEnergyBarrier;
+        Set.lambdaB=AuxlambdaB;
+        Set.Beta=AuxBeta;
+        Set.Bending=AuxBending;
+        Set.BendingAreaDependent=AuxBendingAreaDependent;
+        Set.lambdaBend=AuxlambdaBend;
+        Set.nu=Auxnu;
         break;
     end
     
-    
-    
-    
-
-
 end 
 
 end 

8a16fb344c86fddcaee188b1097bb3abb9fff794
Pablo Vicente Munuera <pablovm1990@gmail.com>
2020-12-16 15:12:58 +0000

    Update KgContractility.m
    Removing unncessary code of Assemblies

--- Kg/KgContractility.m
+++ Kg/KgContractility.m
@@ -47,23 +47,28 @@ for numCell = 1:Cell.n
         %% Calculate residual g
         g_current = computeGContractility(l_i0, l_i, y_1, y_2);
         g = assembleG(g, g_current, edgeLengths(numEdge, 2:3));
-        %% Calculate Jacobian
-        K_current = computeKContractility(l_i0, l_i, y_1, y_2);
         
-        if Set.Sparse
-            [si,sj,sv,sk] = assembleKSparse(K_current, edgeLengths(numEdge, 2:3), si, sj, sv, sk);
-        else
-            K = assembleK(K, K_current, edgeLengths(numEdge, 2:3));
+        %% AssembleK
+        if  nargout>1
+            %% Calculate Jacobian
+            K_current = computeKContractility(l_i0, l_i, y_1, y_2);
+
+            if Set.Sparse
+                [si,sj,sv,sk] = assembleKSparse(K_current, edgeLengths(numEdge, 2:3), si, sj, sv, sk);
+            else
+                K = assembleK(K, K_current, edgeLengths(numEdge, 2:3));
+            end
+
+            %% Calculate energy
+            energy = energy + computeEnergyContractility(l_i, l_i0);
         end
-        
-        %% Calculate energy
-        energy = energy + computeEnergyContractility(l_i, l_i0);
-    end
-    
-    if Set.Sparse
-        K=sparse(si(1:sk),sj(1:sk),sv(1:sk),dimensionsG,dimensionsG);
     end
 end
+
+if Set.Sparse && nargout>1
+    K=sparse(si(1:sk),sj(1:sk),sv(1:sk),dimensionsG,dimensionsG);
+end
+
 end
 
 function [kContractility] = computeKContractility(l_i0, l_i, y_1, y_2)
@@ -91,60 +96,3 @@ end
 function [energyConctratility] = computeEnergyContractility(l_i, l_i0)
     energyConctratility = 0.5 * (l_i - (l_i0))^2;
 end
-
-%%
-function   gAccumulated=assembleG(gAccumulated,gCurrent,nY)
-% Assembles residual of a pair of vertices (6 components)
-dim=3;
-for I=1:length(nY) % loop on 2 vertices of an edge
-    if nY(I)>0
-         idofg=(nY(I)-1)*dim+1:nY(I)*dim; % global dof
-         idofl=(I-1)*dim+1:I*dim;
-         gAccumulated(idofg)=gAccumulated(idofg)+gCurrent(idofl);
-    end
-end
-end
-%%
-function Kaccumulated= assembleK(Kaccumulated,Kcurrent,nY)
-% Assembles volume Jacobian of a triangle of vertices (12x12 components)
-dim=3;
-
-
-for I=1:length(nY)
-    for J=1:length(nY)
-        if nY(I)>0
-            idofg=(nY(I)-1)*dim+1:nY(I)*dim; % global dof
-            idofl=(I-1)*dim+1:I*dim;
-            jdofg=(nY(J)-1)*dim+1:nY(J)*dim; % global dof
-            jdofl=(J-1)*dim+1:J*dim;
-            Kaccumulated(idofg,jdofg)=Kaccumulated(idofg,jdofg)+Kcurrent(idofl,jdofl);
-        end
-    end 
-end
-end
-
-
-%%
-function [si,sj,sv,sk]= assembleKSparse(Kcurrent,nY,si,sj,sv,sk)
-% Assembles Jacobian of a pair of vertices (6x6 components)
-dim=3;
-
-for I=1:length(nY)
-    for J=1:length(nY)
-        if nY(I)>0
-            idofg=(nY(I)-1)*dim+1:nY(I)*dim; % global dof
-            idofl=(I-1)*dim+1:I*dim;
-            jdofg=(nY(J)-1)*dim+1:nY(J)*dim; % global dof
-            jdofl=(J-1)*dim+1:J*dim;
-            for d=1:dim
-                si(sk+1:sk+dim)=idofg;
-                sj(sk+1:sk+dim)=jdofg(d);
-                sv(sk+1:sk+dim)=Kcurrent(idofl,jdofl(d));
-                sk=sk+dim;
-            end
-        end
-    end 
-end
-
-end
-%%

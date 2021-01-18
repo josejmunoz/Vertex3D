5be5fa7f5cbc45551fc09ba9ed619959e0a24aca
Pablo Vicente Munuera <pablovm1990@gmail.com>
2021-01-18 09:31:24 +0000

    Create InputWoundHealing.m

---
+++ InputWoundHealing.m
@@ -0,0 +1,61 @@
+%% geometry
+Set.e=1;  % Example Number look in Geo\Example.m 
+Set.Method=1;
+% Tuning parameters
+Set.s=1.5;
+Set.f=Set.s/2;
+
+%%  Mechanics
+%---------- Volume
+Set.lambdaV=5;
+
+%---------- Surface
+% Set.SurfaceType=4 : Surface-Energy based on the whole cell area differential adhsion
+Set.SurfaceType=4;
+% external 
+Set.lambdaS1=1;
+% Cell-Cell 
+Set.lambdaS2=.5;
+% Cell-substrate
+Set.lambdaS3=.5;
+
+%---------- EnergyBarrier
+Set.EnergyBarrier=true;
+Set.lambdaB=5;
+Set.Beta=1;  
+% WBexp =exp( lambdaB*  ( 1 - Set.Beta*At/At0 )  );   
+
+%--------- Bending 
+Set.Bending=false;
+%------- Viscosity
+Set.nu=0.05;   % this is eta 
+
+%% Remodeling
+Set.Remodelling=false;
+Set.RemodelTol=.5e-6;
+Set.RemodelingFrequency=2;
+
+%% time
+Set.tend=300;
+Set.Nincr=300;
+
+%%  Boundary Displacement 
+Set.BC=2;  %  Compression
+       Set.VFixd=-1;         % Vertices with y-coordinates > Set.VPrescribed are those to be prescribed (pulled)
+       Set.dx=1;             % Total displacement  
+       Set.TStartBC=20;      % The time at which boundary conditions start to be applied    
+       Set.TStopBC=100;       % The time at which boundary conditions are removed 
+    
+%% Contractility
+% Set.Contractility = true;
+% Set.cContractility = 0.03;
+% 
+% 
+% 
+% %% Ablating cells
+% 
+% Set.Ablation = true;
+
+
+
+

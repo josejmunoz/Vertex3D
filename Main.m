6f1ea9da158849c6ed47954a9441ab374085488c
Pablo Vicente Munuera <pablovm1990@gmail.com>
2021-01-28 16:55:59 +0000

    seems to be working
    More tests required

--- Main.m
+++ Main.m
@@ -83,15 +83,24 @@ while t<=Set.tend
     if any(tooSmallCells) % Remove cell in the case is too small
         idsToRemove = Cell.Int(tooSmallCells);
         Cell = Cell.removeCells(tooSmallCells);
-        CellInput.LambdaS1Factor(tooSmallCells) = 0;
-        CellInput.LambdaS2Factor(tooSmallCells) = 0;
-        CellInput.LambdaS3Factor(tooSmallCells) = 0;
-        CellInput.LambdaS4Factor(tooSmallCells) = 0;
+        CellInput.LambdaS1Factor(tooSmallCells) = [];
+        CellInput.LambdaS2Factor(tooSmallCells) = [];
+        CellInput.LambdaS3Factor(tooSmallCells) = [];
+        CellInput.LambdaS4Factor(tooSmallCells) = [];
         XgID = [XgID; idsToRemove];
+        
+        %Remove edges between ghost cell and external nodes. Therefore,
+        %also, remove faces between ghost cell and external nodes and
+        %associated vertices
+        
         %Here it should change interior faces to exterior face from the smaller one
         Faces=Faces.CheckInteriorFaces(XgID);
         Cell.AssembleNodes = Cell.Int;
         [Cell,Faces,nC,SCn,flag32] = ReBuildCells(Cell,T,Y,X,Faces,SCn);
+        
+        
+        % Check consequences of this one:
+        Dofs=GetDOFs(Y,Cell,Faces,Set);
     end
 
     % ----------- Remodel--------------------------------------------------

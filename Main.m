6a5a50f57ac9ec108af8a142dd849aa3614875b8
Pablo Vicente Munuera <pablovm1990@gmail.com>
2021-02-11 15:22:01 +0000

    Update Main.m

--- Main.m
+++ Main.m
@@ -62,7 +62,7 @@ else
 end
 
 %% Loop over time
-tp = 0
+tp = 0;
 tr=0;
 Set.nu0=Set.nu;
 Set.MaxIter0=Set.MaxIter;
@@ -83,28 +83,11 @@ while t<=Set.tend
     %   to be repeated
     Yp=Y; Cellp=Cell;
     Set.iIncr=numStep;
-    % ----------- Apply Boundary Condition --------------------------------
-    if Set.BC==1 && t<=Set.TStopBC && t>=Set.TStartBC && Set.ApplyBC
-        Y.DataRow(Dofs.PrescribedY,2)=Y.DataRow(Dofs.PrescribedY,2)+Set.dx/((Set.TStopBC-Set.TStartBC)/Set.dt);
-        Cell.FaceCentres.DataRow(Dofs.PrescribedS,2)=Cell.FaceCentres.DataRow(Dofs.PrescribedS,2)+Set.dx/((Set.TStopBC-Set.TStartBC)/Set.dt);
-        Dofs.FreeDofs(ismember(Dofs.FreeDofs,Dofs.dofP))=[];
-        Dofs.FreeDofs(ismember(Dofs.FreeDofs,Dofs.dofC))=[];
-        
-    elseif Set.BC==2 && t<=Set.TStopBC && t>=Set.TStartBC && Set.ApplyBC
-        Set.WallPosition=max([Y.DataRow(:,2);Cell.FaceCentres.DataRow(:,2)]);
-        [Dofs,Set]=GetWallBoundaryCondition(Set,Y,Cell,Faces);
-        Y.DataRow(Dofs.PrescribedY,2)=Set.WallPosition;
-        Cell.FaceCentres.DataRow(Dofs.PrescribedS,2)=Set.WallPosition;
-        Dofs.FreeDofs(ismember(Dofs.FreeDofs,Dofs.dofP))=[];
-        Dofs.FreeDofs(ismember(Dofs.FreeDofs,Dofs.dofC))=[];
-    elseif Set.BC==1 || Set.BC==2
-        Dofs.FreeDofs=unique([Dofs.FreeDofs Dofs.dofC Dofs.dofP]);
-    end
     
-    Ytn=[Yn.DataOrdered ;SCn.DataOrdered];
-    Yt=[Y.DataOrdered ;Cell.FaceCentres.DataOrdered];
-    y=reshape(Yt',Set.NumTotalV*3,1);
-    yn=reshape(Ytn',Set.NumTotalV*3,1);
+    % ----------- Apply Boundary Condition --------------------------------
+    [Cell, Y, Dofs, Yt, Ytn, y, yn] = applyBoundaryCondition(t, Y, Set, Cell, Dofs)
+    
+    
     % ----------- Compute K, g ---------------------------------------
     [g,K,Cell,Energy]=KgGlobal(Cell,Faces,Y,y,yn,Set,CellInput,XgSub);
     dy=zeros(size(y));
@@ -152,7 +135,9 @@ while t<=Set.tend
                 || abs((gr0-gr)./gr0)>1e3
             Set.iter=Set.MaxIter;
         end
-    end%=================================================================
+    end
+    
+    %=================================================================
 
 
     if Set.iter == Set.MaxIter0 &&  (gr>Set.tol || dyr>Set.tol)

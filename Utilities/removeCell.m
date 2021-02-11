e2d7d3e577c80d62b695ed3424870d8a7dd200df
Pablo Vicente Munuera <pablovm1990@gmail.com>
2021-02-11 16:23:08 +0000

    Moving file and refactor

---
+++ Utilities/removeCell.m
@@ -0,0 +1,26 @@
+function [Cell, CellInput, XgID, Faces,nC,SCn,flag32, Dofs] = removeCell(Cell, CellInput, XgID, Faces, T, Y, X, SCn)
+%REMOVECELLDEPENDINGVOL Summary of this function goes here
+%   Detailed explanation goes here
+
+idsToRemove = Cell.Int(tooSmallCells);
+Cell = Cell.removeCells(tooSmallCells);
+CellInput.LambdaS1Factor(tooSmallCells) = [];
+CellInput.LambdaS2Factor(tooSmallCells) = [];
+CellInput.LambdaS3Factor(tooSmallCells) = [];
+CellInput.LambdaS4Factor(tooSmallCells) = [];
+XgID = [XgID; idsToRemove];
+
+%Remove edges between ghost cell and external nodes. Therefore,
+%also, remove faces between ghost cell and external nodes and
+%associated vertices
+
+%Here it should change interior faces to exterior face from the smaller one
+Faces=Faces.CheckInteriorFaces(XgID);
+Cell.AssembleNodes = Cell.Int;
+[Cell,Faces,nC,SCn,flag32] = ReBuildCells(Cell,T,Y,X,Faces,SCn);
+
+% Check consequences of this one:
+Dofs=GetDOFs(Y,Cell,Faces,Set);
+
+end
+

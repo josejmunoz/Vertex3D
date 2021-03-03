f1c9e50a0b1ff872208889fda71893f1d2eeefb7
Pablo Vicente Munuera <pablovm1990@gmail.com>
2021-03-03 16:01:40 +0000

    Updating also volume of debris cell

--- Utilities/updateParametersOnTime.m
+++ Utilities/updateParametersOnTime.m
@@ -1,25 +1,29 @@
-function [Set] = updateContractilityOnTime(currentT, Set, Cell)
-%UPDATECONTRACTILITYONTIME Summary of this function goes here
+function [Set] = updateParametersOnTime(currentT, Set, Cell)
+%UPDATEPARAMETERSONTIME Summary of this function goes here
 %   Detailed explanation goes here
-%% Contractility time dependent
+
 if any(Cell.GhostCells)
+    %% Contractility time dependent
     if currentT > (Set.TAblation + Set.initMidEndContractilityTime_PurseString(2))
         Set.cPurseString = Set.cPurseString_MidEndTimeDependent(currentT) + Set.initMidEndContractility_PurseString(2);
     else
         Set.cPurseString = Set.cPurseString_InitMidTimeDependent(currentT) + Set.initMidEndContractility_PurseString(1);
     end
-else
-    Set.cPurseString = 0;
-end
-
-if any(Cell.GhostCells)
+    
     if currentT > (Set.TAblation + Set.initMidEndContractilityTime_LateralCables(2))
         Set.cLateralCables = Set.cLateralCables_MidEndTimeDependent(currentT) + Set.initMidEndContractility_LateralCables(2);
     else
         Set.cLateralCables = Set.cLateralCables_InitMidTimeDependent(currentT) + Set.initMidEndContractility_LateralCables(1);
     end
+    
+    %% Volume degradation
+    if currentT < (Set.TAblation + Set.TToCompleteAblation)
+        Set.lambdaV_Debris = Set.lambdaV_DebrisTime(currentT);
+    end
 else
+    Set.cPurseString = 0;
     Set.cLateralCables = 0;
+    Set.lambdaV_Debris = Set.lambdaV;
 end
 end
 

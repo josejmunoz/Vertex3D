a91f3bb5e4435456afd996d7591e46b7aeb7fd8d
Pablo Vicente Munuera <pablovm1990@gmail.com>
2021-03-03 15:29:25 +0000

    Renaming

--- Utilities/contractilityFunction.m
+++
@@ -1,7 +0,0 @@
-function [contractilityTimeDependent] = contractilityFunction(initC_Contractility, endC_Contractility, initT_Contractility, endT_Contractility, Set)
-%CONTRACTILITYFUNCTION Summary of this function goes here
-%   Detailed explanation goes here
-    slope1 = (endC_Contractility - initC_Contractility) / (endT_Contractility - initT_Contractility);
-    contractilityTimeDependent = @(timePoint) (slope1 * (timePoint - Set.TAblation - initT_Contractility)); %initC_Contractility;
-end
-
---
+++ Utilities/functionVariableOnTime.m
@@ -0,0 +1,7 @@
+function [contractilityTimeDependent] = functionVariableOnTime(initValue, endValue, initTime, endTime, Set)
+%FUNCTIONVARIABLEONTIME Summary of this function goes here
+%   Detailed explanation goes here
+    slope1 = (endValue - initValue) / (endTime - initTime);
+    contractilityTimeDependent = @(timePoint) (slope1 * (timePoint - Set.TAblation - initTime)); %initC_Contractility;
+end
+

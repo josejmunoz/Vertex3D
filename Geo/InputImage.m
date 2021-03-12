c5313c184a54fddbb18695396bce8c783bd0f435
Pablo Vicente Munuera <pablovm1990@gmail.com>
2021-03-12 10:00:27 +0000

    Update InputImage.m
    Comments

--- Geo/InputImage.m
+++ Geo/InputImage.m
@@ -97,8 +97,6 @@ nonEmptyCells = cellfun(@isempty, verticesInfo.edges) == 0;
 nonEmptyCells = zeros(size(nonEmptyCells)) == 1;
 nonEmptyCells(cellIdsAsInternal) = 1;
 
-totalRegularCells = sum(nonEmptyCells);
-
 X = horzcat(faceCentresVertices, zeros(size(nonEmptyCells)));
 
 vertex2D = verticesInfo.location / imgSize;
@@ -123,18 +121,9 @@ X_topVerticesIds = X_topIds(size(XgTopFaceCentre, 1)+1:end);
 X = vertcat(X, X_topNodes);
 
 
-% % Lateral boundary ghost cells
-% XgBoundary_1 = horzcat(vertcat(faceCentres(nonEmptyCells == 0).Centroid), repmat(cellHeight/4, sum(nonEmptyCells == 0), 1));
-% XgBoundary_2 = horzcat(vertcat(faceCentres(nonEmptyCells == 0).Centroid), repmat(-cellHeight/4, sum(nonEmptyCells == 0), 1));
-% X = vertcat(X, XgBoundary_1, XgBoundary_2);
-
-
 % Difference cell nodes and ghost nodes
 xInternal = find(nonEmptyCells);
 XgID = horzcat(find(nonEmptyCells == 0)', (length(faceCentresVertices)+1):size(X, 1));
-% totalRegularCells = sum(nonEmptyCells);
-% xInternal = 1:totalRegularCells;
-% XgID = (totalRegularCells+1):size(X, 1);
 
 %% Create tetrahedra
 trianglesConnectivity = verticesInfo.connectedCells;
@@ -142,39 +131,10 @@ trianglesConnectivity = verticesInfo.connectedCells;
 [Twg_top] = createTetrahedra(trianglesConnectivity, neighboursNetwork, verticesInfo.edges, X, xInternal, X_topNodes, X_topFaceIds, X_topVerticesIds);
 
 Twg = vertcat(Twg_top, Twg_bottom);
-% X = horzcat(faceCentresVertices, zeros(size(nonEmptyCells)));
-% Twg = delaunay(vertcat(X, XgTopFaceCentre, XgBottomFaceCentre));
-
-% for numVertex = 1:length(X_topVerticesIds)
-%     ismember(Twg_top, X_topVerticesIds(numVertex))
-% end
 
 %% Filtering of unnecessary nodes
 % Remove Ghost tets 
 Twg(all(ismember(Twg,XgID),2),:)=[];
-
-% % Remove addition nodes- not used
-% newTwg=zeros(size(Twg));
-% newX=zeros(size(X));
-% newXgID=zeros(size(X,1),1);
-% aux1=zeros(size(X,1),1);
-% aux2=1;
-% aux3=1;
-% for i=1:size(X,1)
-%     if ismember(i,Twg)
-%         newTwg(ismember(Twg,i))=aux2;
-%         newX(aux2,:)=X(i,:);
-%         aux1(i)=aux2;
-%         if ismember(i,XgID)
-%             newXgID(aux3)=aux2;
-%             aux3=aux3+1;
-%         end
-%         aux2=aux2+1;
-%     end
-% end
-% X=newX(1:aux2-1,:);
-% XgID=newXgID(1:aux3-1);
-% Twg=newTwg;
 
 %Centre Nodal position at (0,0)
 X(:,1)=X(:,1)-mean(X(:,1));
@@ -183,21 +143,6 @@ X(:,3)=X(:,3)-mean(X(:,3));
 
 %% Create vertices Y
 Y=DynamicArray(size(verticesInfo.location, 1)*3, 3);
-
-% Top vertices:
-% x,y: imgNeighbours; z:cellHeight/2
-Y = Y.Add([vertex2D, repmat(cellHeight/2, size(vertex2D, 1), 1)]);
-
-% Bottom vertices:
-% x,y: imgNeighbours; z:-cellHeight/2
-Y = Y.Add([vertex2D, repmat(-cellHeight/2, size(vertex2D, 1), 1)]);
-
-% % Vertices of faces in depth:
-% % x,y: mean([vertex1, vertex2], [], 2); z: 0
-% allEdges = vertcat(verticesInfo.edges{:});
-% allEdgesVertices = verticesInfo.location(allEdges, :);
-% allEdgesVertices = round(horzcat(mean(horzcat(allEdgesVertices(1:2:size(allEdgesVertices, 1), 1), allEdgesVertices(2:2:size(allEdgesVertices, 1), 1)), 2), mean(horzcat(allEdgesVertices(1:2:size(allEdgesVertices, 1), 2), allEdgesVertices(2:2:size(allEdgesVertices, 1), 2)), 2)));
-% Y = Y.Add([allEdgesVertices, zeros(size(allEdgesVertices, 1), 1)]);
 
 %Y_new=GetYFromX(X,XgID,Twg,cellHeight/2);
 Y_new = zeros(size(Twg, 1), 3);
@@ -237,17 +182,11 @@ Yt=[Y.DataOrdered ;Cell.FaceCentres.DataOrdered];
 T=DynamicArray(ceil(size(Twg,1)*1.5),size(Twg,2));
 T=T.Add(Twg);
 
-
-% Regularize small Triangles (Uncomment this line if there are very small triangles in the initial mesh)
-% [Y,Cell,Faces,Yn,SCn]=RegularizeMesh(Y,Cell,Faces,Set,Yn,SCn);
-
-
 Set.BarrierTri0=realmax; 
 for i=1:Cell.n
     Set.BarrierTri0=min([Cell.SAreaTri{i}; Set.BarrierTri0]);
 end
 Set.BarrierTri0=Set.BarrierTri0/10;
-
 
 [Cell,Faces,Y]=CheckOrderingOfTriangulaiton(Cell,Faces,Y,Set);
 

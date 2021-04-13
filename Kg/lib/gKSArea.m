bfd7b3013f0b55b61e54d9f45439f7a6b876daaf
Pablo Vicente Munuera <pablovm1990@gmail.com>
2021-04-13 15:53:07 +0100

    Reducing the number of calls to Cross

--- Kg/lib/gKSArea.m
+++ Kg/lib/gKSArea.m
@@ -3,11 +3,16 @@ function [gs,Ks,Kss]=gKSArea(y1,y2,y3)
 % notation
 % YI=Cross_mex(yI)
 % q =(y1-y3)x(y2-y3) = Y1y2 - Y1y3 - Y3y2
-q= Cross_mex(y2)*y1' - Cross_mex(y2)*y3' + Cross_mex(y1)*y3';
+
+y1_Crossed = Cross_mex(y1);
+y2_Crossed = Cross_mex(y2);
+y3_Crossed = Cross_mex(y3);
+
+q= y2_Crossed*y1' - y2_Crossed*y3' + y1_Crossed*y3';
 % Q_I = der_yI q =  Cross_mex(yK) -Cross_mex(yJ)
-Q1=Cross_mex(y2)-Cross_mex(y3);
-Q2=Cross_mex(y3)-Cross_mex(y1);
-Q3=Cross_mex(y1)-Cross_mex(y2);
+Q1=y2_Crossed-y3_Crossed;
+Q2=y3_Crossed-y1_Crossed;
+Q3=y1_Crossed-y2_Crossed;
 % KK_IJ = der_yJ QI
 fact=1/(2*norm(q));
 gs=fact.*[Q1'*q; % der_Y1 (det(Y1,Y2,Y3))
@@ -15,8 +20,9 @@ gs=fact.*[Q1'*q; % der_Y1 (det(Y1,Y2,Y3))
     Q3'*q];
 
 Kss=-(2/norm(q)).*(gs)*(gs');
-Ks=fact.*[Q1'*Q1               KK(1,2,3,y1,y2,y3) KK(1,3,2,y1,y2,y3);
-    KK(2,1,3,y1,y2,y3)  Q2'*Q2              KK(2,3,1,y1,y2,y3);
-    KK(3,1,2,y1,y2,y3)  KK(3,2,1,y1,y2,y3) Q3'*Q3            ];
+
+Ks=fact.*[Q1'*Q1               KK(y1_Crossed,y2_Crossed,y3_Crossed,y1,y2,y3) KK(y1_Crossed,y3_Crossed,y2_Crossed,y1,y3,y2);
+    KK(y2_Crossed,y1_Crossed,y3_Crossed,y2,y1,y3)  Q2'*Q2              KK(y2_Crossed,y3_Crossed,y1_Crossed,y2,y3,y1);
+    KK(y3_Crossed,y1_Crossed,y2_Crossed,y3,y1,y2)  KK(y3_Crossed,y2_Crossed,y1_Crossed,y3,y2,y1) Q3'*Q3            ];
 end
 

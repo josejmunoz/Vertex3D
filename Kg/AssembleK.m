a0142e0294fcb1dcf8c9dc7daac9ba855ae37aa1
Pablo Vicente Munuera <pablovm1990@gmail.com>
2021-04-16 11:44:34 +0100

    Improving assembleg also

--- Kg/AssembleK.m
+++ Kg/AssembleK.m
@@ -1,4 +1,4 @@
-function K= AssembleK(K,Ke,nY)
+function K = AssembleK(K,Ke,nY)
 % Assembly of the Jacobian of an element (e.g. Triangle ->length(nY)=3 size(Ke)=9x9,
 %                                              edge->   length(nY)=2  size(Ke)=6x6)
 dim=3;
@@ -6,8 +6,8 @@ idofg = zeros(length(nY), 1);
 jdofg = idofg;
 
 for I = 1:length(nY)
-    idofg((1:dim) + (I-1)*dim) = (nY(I)-1)*dim+1:nY(I)*dim;
-    jdofg((1:dim) + (I-1)*dim) = (nY(I)-1)*dim+1:nY(I)*dim; % global dof
+    idofg((I-1)*dim+1:I*dim) = (nY(I)-1)*dim+1:nY(I)*dim;
+    jdofg((I-1)*dim+1:I*dim) = (nY(I)-1)*dim+1:nY(I)*dim; % global dof
 end
 
 K(idofg,jdofg) = K(idofg,jdofg) + Ke;

7a513be1daeb63994778029c37478aa1551276e7
Pablo Vicente Munuera <pablovm1990@gmail.com>
2021-05-06 09:11:26 +0100

    Update gKBulk.m
    inititate function

--- Kg/lib/gKBulk.m
+++ Kg/lib/gKBulk.m
@@ -7,5 +7,11 @@ function [g, K] = gKBulk(y1, y2, y3, y4)
 %   g: 4x3 matrix (4 vertices x 3 coordinates)
 %   K: 12x12 matrix
 
+dim = 3;
+
+g = zeros(4, dim);
+
+K = zeros(numel(g), numel(g));
+
 end
 

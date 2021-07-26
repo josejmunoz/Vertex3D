8dd95a23b73e00cd6aaaef5a59f43b2180233cc1
Pablo Vicente Munuera <pablovm1990@gmail.com>
2021-07-26 13:58:25 +0100

    checking if the same tetrahedron is already in the list

--- Utilities/CheckConvexityCondition.m
+++ Utilities/CheckConvexityCondition.m
@@ -1,18 +1,22 @@
-function [IsNotConvex,itet]=CheckConvexityCondition(Tnew,T)
+function [IsConvex, tetID]=CheckConvexityCondition(Tnew,Tetrahedra, X)
 %CHECKCONVEXITYCONDITION Summary of this function goes here
-%   Detailed explanation goes here
+%   Check if the tetrahedron:
+%   - is already created
+%   - overlap with other tetrahedra
+%   - is convex
 
-IsNotConvex=false;
-itet=[];
-for i=1:T.n
-    Tlogical=ismember(Tnew,T.DataRow(i,:));
-    Tlogical=all(Tlogical,2);
-    if any(Tlogical)
-       IsNotConvex=true;
-       itet=i;
-       return
-    end 
-end 
+%% Checking if the same tetrahadron is already on T
+[foundTets, tetFoundIds] = ismember(sort(Tnew, 2),sort(Tetrahedra.DataRow, 2), 'rows');
+if any(foundTets>0)
+    tetID = tetFoundIds(foundTets);
+    IsConvex = true;
+    return
+end
+
+%% Checking if Tnew overlap with other tetrahedra
+
+
+%% Checking if Tnew is convex
 
 end
 

e4695c515b8d430d5a76846e29b3df4de8d10c2b
Pablo Vicente Munuera <pablovm1990@gmail.com>
2021-09-06 14:52:51 +0100

    Checking convexity condition and solving concavities

--- Src/convexify.m
+++ Src/convexify.m
@@ -1,6 +1,40 @@
-function [newXs] = convexify(tetrahedra,X)
+function [newXs] = convexify(tetrahedra, X, concaveTetVertices)
 %CONVEXIFY Convexify tetrahedra
 %   Detailed explanation goes here
-
+    newXs = X;
+    for numPairOfTets = 1:size(concaveTetVertices, 1)
+        %Order is: [endPointCurrentTet endPointNextTet edgeVertices theOtherVertex]
+        currentVertices = concaveTetVertices(numPairOfTets, :);
+        
+        %% Method 1: Increase the height of the 'edge vertices'
+        outerVertices = mean(newXs(currentVertices(1:2), :));
+        edgeVertices = mean(newXs(currentVertices(3:4), :));
+        
+        difference = outerVertices - edgeVertices;
+        
+        %Modify the outer vertices to make them a 
+        newXs(currentVertices(1:2), :) = newXs(currentVertices(1:2), :) - difference;
+        
+        Tnew = tetrahedra(sum(ismember(tetrahedra, currentVertices(1:2)), 2)>3, :);
+        %Check if the current Tet is now convex
+        if CheckConvexityCondition(Tnew, tetrahedra, newXs, false)
+            % We go back to the old vertices position
+            newXs(currentVertices(1:2), :) = X(currentVertices(1:2), :);
+        end
+        
+        Tnew = tetrahedra(sum(ismember(tetrahedra, currentVertices(1:2)), 2)>0, :);
+        [isConvex_new, concaveTetVertices_new] = CheckConvexityCondition(Tnew, tetrahedra, newXs, false);
+        [isConvex_old, concaveTetVertices_old] = CheckConvexityCondition(Tnew, tetrahedra, X, false);
+        if size(concaveTetVertices_new, 1) <= size(concaveTetVertices_old, 1)
+            newXs(currentVertices(1:2), :) = X(currentVertices(1:2), :);
+        else % Current tetrahedron is now convex
+            continue;
+        end
+        %% Method 2: Move the other vertices 
+        sum(ismember(K.ConnectivityList, tetrahedra, 'rows'))
+        
+        
+    end
+    disp('Convexify - done');
 end
 
--- Utilities/CheckConvexityCondition.m
+++ Utilities/CheckConvexityCondition.m
@@ -1,4 +1,4 @@
-function [isConvex, tetID]=CheckConvexityCondition(Tnew,Tetrahedra, X)
+function [isConvex, concaveTetVertices]=CheckConvexityCondition(Tnew,Tetrahedra, X, performConvexify)
 %CHECKCONVEXITYCONDITION Summary of this function goes here
 %   Check if the tetrahedron:
 %   - is already created
@@ -6,18 +6,22 @@ function [isConvex, tetID]=CheckConvexityCondition(Tnew,Tetrahedra, X)
 %   - is convex
 
 isConvex = true;
-tetID = [];
+concaveTetVertices = [];
+
+if exist('performConvexify', 'var') == 0
+    performConvexify = 1;
+end
 
 %% Checking if the same tetrahadron is already on T
-if isempty(Tnew) == 0
-    [foundTets, tetFoundIds] = ismember(sort(Tnew, 2),sort(Tetrahedra.DataRow, 2), 'rows');
+if isempty(Tnew) == 0 && performConvexify == 1
+    [foundTets, tetFoundIds] = ismember(sort(Tnew, 2),sort(Tetrahedra, 2), 'rows');
     if any(foundTets>0)
-        tetID = tetFoundIds(foundTets);
+        concaveTetVertices = tetFoundIds(foundTets);
         isConvex = false;
         return
     end
-else
-    Tnew = Tetrahedra.DataRow;
+elseif isempty(Tnew)
+    Tnew = Tetrahedra;
 end
 
 %% Checking if Tnew overlap with other tetrahedra
@@ -61,61 +65,17 @@ for numTet = 1:size(Tnew, 1)
 %                 plot(a, 'FaceAlpha', 0.5)
 %                 close(h)
                 isConvex = false;
-                tetID = [tetID; numTet, nextNumTet];
+                concaveTetVertices = [concaveTetVertices; tetVertices];
             end
-            
-            
-%             vectorToOtherVertex_Current = X(endPointCurrentTet, :) - X(theOtherVertex, :);
-%             vectorToOtherVertex_Next = X(endPointNextTet, :) - X(theOtherVertex, :);
-% 
-%             % Here we create two normals of the triangles cross of two
-%             % vectors represents its normal
-%             normalNextTriangle = cross(X(edge(2), :) - X(endPointNextTet, :), X(edge(1), :) - X(endPointNextTet, :));
-%             normalCurrentTriangle = cross(X(edge(2), :) - X(endPointCurrentTet, :), X(edge(1), :) - X(endPointCurrentTet, :));
-%             
-%             if dot(vectorToOtherVertex_Current, normalCurrentTriangle) / (norm(vectorToOtherVertex_Current) *  norm(normalCurrentTriangle)) > 0 
-%                 normalCurrentTriangle = -normalCurrentTriangle;
-%             end
-%             
-%             if dot(vectorToOtherVertex_Next, normalNextTriangle) / (norm(vectorToOtherVertex_Next) *  norm(normalNextTriangle)) > 0 
-%                 normalNextTriangle = -normalNextTriangle;
-%             end
-%             
-%             h = figure;
-%             plot3(X([endPointCurrentTet endPointNextTet], 1), X([endPointCurrentTet endPointNextTet], 2), X([endPointCurrentTet endPointNextTet], 3), 'x')
-%             hold on, quiver3(X(endPointNextTet, 1), X(endPointNextTet, 2), X(endPointNextTet, 3), normalNextTriangle(1), normalNextTriangle(2), normalNextTriangle(3));
-%             hold on, quiver3(X(endPointCurrentTet, 1), X(endPointCurrentTet, 2), X(endPointCurrentTet, 3), normalCurrentTriangle(1), normalCurrentTriangle(2), normalCurrentTriangle(3));
-%             hold on, plot3(X(edge, 1), X(edge, 2), X(edge, 3), 'bo')
-%             hold on, plot3(X(theOtherVertex, 1), X(theOtherVertex, 2), X(theOtherVertex, 3), 'ro')
-%             
-%             parallelepidVectors = [normalCurrentTriangle; normalNextTriangle; X(edge(1), :) - X(edge(2), :)];
-% %             minValueAllowed = 0.000001;
-% %             minValues = min(parallelepidVectors);
-% %             parallelepidVectors(:, minValues < minValueAllowed) = parallelepidVectors(:, minValues < minValueAllowed) + (minValueAllowed - minValues(minValues < minValueAllowed));
-%             det(parallelepidVectors)
-%             edge
-%             close(h)
-%             if det(parallelepidVectors) < 0
-%                 h = figure;
-%                 plot3(X([endPointCurrentTet endPointNextTet], 1), X([endPointCurrentTet endPointNextTet], 2), X([endPointCurrentTet endPointNextTet], 3), 'x')
-%                 hold on, quiver3(X(endPointNextTet, 1), X(endPointNextTet, 2), X(endPointNextTet, 3), normalNextTriangle(1), normalNextTriangle(2), normalNextTriangle(3));
-%                 hold on, quiver3(X(endPointCurrentTet, 1), X(endPointCurrentTet, 2), X(endPointCurrentTet, 3), normalCurrentTriangle(1), normalCurrentTriangle(2), normalCurrentTriangle(3));
-%                 hold on, plot3(X(edge, 1), X(edge, 2), X(edge, 3), 'bo')
-%                 hold on, plot3(X(theOtherVertex, 1), X(theOtherVertex, 2), X(theOtherVertex, 3), 'ro')
-%                 a = alphaShape(X([endPointCurrentTet endPointNextTet edge theOtherVertex], 1), X([endPointCurrentTet endPointNextTet edge theOtherVertex], 2), X([endPointCurrentTet endPointNextTet edge theOtherVertex], 3));
-%                 plot(a)
-%                 close(h)
-%                 isConvex = false;
-%                 tetID = [tetID; numTet, nextNumTet];
-%             end
         end
     end
 end
 
 if isConvex
     disp('All tetrahedra is convex');
-elseif isequal(Tnew, Tetrahedra)
+elseif performConvexify
     %% Need to convexify
+    [newXs] = convexify(Tnew, X, concaveTetVertices);
 end
 
 end

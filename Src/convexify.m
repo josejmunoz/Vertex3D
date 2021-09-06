9f2da9060ef3452cdcd21e1b0316c3766455bd61
Pablo Vicente Munuera <pablovm1990@gmail.com>
2021-09-06 15:39:19 +0100

    Refactor and bugfix

--- Src/convexify.m
+++ Src/convexify.m
@@ -13,22 +13,21 @@ function [newXs] = convexify(tetrahedra, X, concaveTetVertices)
         difference = outerVertices - edgeVertices;
         
         %Modify the outer vertices to make them a 
-        newXs(currentVertices(1:2), :) = newXs(currentVertices(1:2), :) - difference;
+        indicesToCheck = 1:2;
+        newXs(currentVertices(indicesToCheck), :) = newXs(currentVertices(indicesToCheck), :) - difference;
         
-        Tnew = tetrahedra(sum(ismember(tetrahedra, currentVertices(1:2)), 2)>3, :);
-        %Check if the current Tet is now convex
-        if CheckConvexityCondition(Tnew, tetrahedra, newXs, false)
-            % We go back to the old vertices position
-            newXs(currentVertices(1:2), :) = X(currentVertices(1:2), :);
+        [newXs, isConvex] = checkCurrentTetIsNowConvex(newXs, tetrahedra, currentVertices, X, indicesToCheck);
+        
+        if isConvex
+            continue 
         end
         
-        Tnew = tetrahedra(sum(ismember(tetrahedra, currentVertices(1:2)), 2)>0, :);
-        [isConvex_new, concaveTetVertices_new] = CheckConvexityCondition(Tnew, tetrahedra, newXs, false);
-        [isConvex_old, concaveTetVertices_old] = CheckConvexityCondition(Tnew, tetrahedra, X, false);
-        if size(concaveTetVertices_new, 1) <= size(concaveTetVertices_old, 1)
-            newXs(currentVertices(1:2), :) = X(currentVertices(1:2), :);
-        else % Current tetrahedron is now convex
-            continue;
+        indicesToCheck = 3:4;
+        newXs(currentVertices(indicesToCheck), :) = newXs(currentVertices(indicesToCheck), :) + difference;
+        
+        [newXs, isConvex] = checkCurrentTetIsNowConvex(newXs, tetrahedra, currentVertices, X, indicesToCheck);
+        if isConvex
+            continue 
         end
         %% Method 2: Move the other vertices 
         sum(ismember(K.ConnectivityList, tetrahedra, 'rows'))
@@ -36,5 +35,29 @@ function [newXs] = convexify(tetrahedra, X, concaveTetVertices)
         
     end
     disp('Convexify - done');
+end
+
+function [newXs, isConvex] = checkCurrentTetIsNowConvex(newXs, tetrahedra, currentVertices, X, indicesToCheck)
+%% Check if the current Tet is now convex and has not added more concavities
+
+isConvex = false;
+
+Tnew = tetrahedra(sum(ismember(tetrahedra, currentVertices), 2)>3, :);
+if CheckConvexityCondition(Tnew, tetrahedra, newXs, false)
+    % We go back to the old vertices position
+    newXs(currentVertices(indicesToCheck), :) = X(currentVertices(indicesToCheck), :);
+else
+    return
+end
+
+Tnew = tetrahedra(sum(ismember(tetrahedra, currentVertices(indicesToCheck)), 2)>0, :);
+[isConvex_new, concaveTetVertices_new] = CheckConvexityCondition(Tnew, tetrahedra, newXs, false);
+[isConvex_old, concaveTetVertices_old] = CheckConvexityCondition(Tnew, tetrahedra, X, false);
+if size(concaveTetVertices_new, 1) >= size(concaveTetVertices_old, 1)
+    newXs(currentVertices(indicesToCheck), :) = X(currentVertices(indicesToCheck), :);
+else % Current tetrahedron is now convex
+    isConvex = true;
+end
+
 end
 

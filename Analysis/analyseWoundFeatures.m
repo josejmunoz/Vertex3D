2fcb01425e4583f1b357ecbee57dc4a34b6e9b82
Pablo Vicente Munuera <pablovm1990@gmail.com>
2021-12-16 10:16:28 +0000

    analysing with reference

--- Analysis/analyseWoundFeatures.m
+++ Analysis/analyseWoundFeatures.m
@@ -1,4 +1,4 @@
-function [] = analyseWoundFeatures(dirToAnalyse)
+function [woundData, paramsPerFile] = analyseWoundFeatures(dirToAnalyse)
 %WOUNDFEATURES Summary of this function goes here
 %   Detailed explanation goes here
     allSetMat = dir(fullfile(dirToAnalyse, '**/Analysis/cellInfo_42.mat'));
@@ -14,7 +14,8 @@ function [] = analyseWoundFeatures(dirToAnalyse)
         load(strrep(allSetMat(numFile).folder, 'Analysis', 'set.mat'))
         paramsPerFile(numFile, 1:5) = [Set.lambdaV, Set.lambdaS1, Set.mu_bulk, Set.lambda_bulk, Set.cLineTension];
     end
-    woundData = table(maxRecoilingT6(:) - 1.65, apicalWoundAreaT30(:) - 0.52, cellShorteningT30(:) - 0.023);
+    woundData = table(maxRecoilingT6(:), apicalWoundAreaT30(:), cellShorteningT30(:));
+    woundData(end+1, :) = {1.65 0.52 0.023};
     woundData.Properties.VariableNames = {'maxRecoilingT6', 'apicalWoundAreaT30', 'cellShorteningT30'};
     heatmap(woundData, 'cellShorteningT30', 'apicalWoundAreaT30', 'ColorVariable', 'maxRecoilingT6');
 end

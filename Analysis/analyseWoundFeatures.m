a61d92eb21aee7900e0bca665fd50834c2629e65
Pablo <pablovm1990@gmail.com>
2022-02-17 10:34:04 +0000

    Analysis of curve evolution

--- Analysis/analyseWoundFeatures.m
+++ Analysis/analyseWoundFeatures.m
@@ -1,35 +1,59 @@
-function [woundData, paramsPerFile] = analyseWoundFeatures(dirToAnalyse)
+function [woundData, paramsPerFile] = analyseWoundFeatures(dirToAnalyse, evolutionAnalysis)
 %WOUNDFEATURES Summary of this function goes here
 %   Detailed explanation goes here
-    allSetMat = dir(fullfile(dirToAnalyse, '**/Analysis/cellInfo_42.mat'));
-    for numFile = 1:length(allSetMat)
-        load(fullfile(allSetMat(numFile).folder, allSetMat(numFile).name))
-        nonWoundedIterations = find(cellfun(@isempty, woundFeatures));
-        startingTimePoint = nonWoundedIterations(end) + 1;
-        reference = woundFeatures{startingTimePoint};
-        if startingTimePoint ~= 11
-            warning('%s may be wrong, check number %d', fullfile(allSetMat(numFile).folder), numFile);
+    if evolutionAnalysis
+        woundData = [];
+        paramsPerFile = [];
+        dirFiles = dir(dirToAnalyse);
+        nameFiles = {};
+        for numDir = 1:length(dirFiles)
+            infoFiles = dir(fullfile(dirFiles(numDir).folder, dirFiles(numDir).name, '/Analysis/cellInfo_*'));
+            if length(infoFiles) < 25
+                continue
+            end
+            load(fullfile(dirFiles(numDir).folder, dirFiles(numDir).name, strcat('/Analysis/cellInfo_', num2str(length(infoFiles)), '.mat')));
+            woundedFeatures = woundFeatures(21:end);
+            woundedFeaturesOnly = vertcat(woundedFeatures{:});
+            apicalArea = woundedFeaturesOnly.wound2DApicalArea;
+            load(fullfile(dirFiles(numDir).folder, dirFiles(numDir).name, 'set.mat'));
+            timePoints = (1:length(woundedFeaturesOnly.wound2DApicalArea))*(Set.tend/Set.Nincr*1000);
+            plot(timePoints-timePoints(1), apicalArea/apicalArea(1))
+            hold on;
+            nameFiles{end+1} = dirFiles(numDir).name;
         end
-        currentWoundArea = woundFeatures{startingTimePoint + 6};
-        maxRecoilingT6(numFile) = currentWoundArea.wound2DApicalArea / reference.wound2DApicalArea;
-        
-        currentWoundArea = woundFeatures{startingTimePoint + 30};
-        apicalWoundAreaT30(numFile) = currentWoundArea.wound2DApicalArea / reference.wound2DApicalArea;
-        cellShorteningT30(numFile) = reference.apicalIndentionAvg - currentWoundArea.apicalIndentionAvg;
-        
-        load(strrep(allSetMat(numFile).folder, 'Analysis', 'set.mat'))
-        paramsPerFile(numFile, :) = [Set.lambdaV, Set.lambdaV_Debris, Set.lambdaS1, Set.lambdaS2, Set.lambdaS3, Set.LambdaSFactor_Debris, Set.mu_bulk, Set.lambda_bulk, Set.cLineTension, Cell.EdgeLengths0_average, Cell.EdgeLengths0_lateralAverage, Set.kSubstrate, Set.TotalCells, max(Set.cellsToAblate), numFile];
+        legend(nameFiles);
+        nameFiles
+    elseif evolutionAnalysis == 0
+        allSetMat = dir(fullfile(dirToAnalyse, '**/Analysis/cellInfo_42.mat'));
+        for numFile = 1:length(allSetMat)
+            load(fullfile(allSetMat(numFile).folder, allSetMat(numFile).name))
+            nonWoundedIterations = find(cellfun(@isempty, woundFeatures));
+            startingTimePoint = nonWoundedIterations(end) + 1;
+            reference = woundFeatures{startingTimePoint};
+            if startingTimePoint ~= 11
+                warning('%s may be wrong, check number %d', fullfile(allSetMat(numFile).folder), numFile);
+            end
+            currentWoundArea = woundFeatures{startingTimePoint + 6};
+            maxRecoilingT6(numFile) = currentWoundArea.wound2DApicalArea / reference.wound2DApicalArea;
+
+            currentWoundArea = woundFeatures{startingTimePoint + 30};
+            apicalWoundAreaT30(numFile) = currentWoundArea.wound2DApicalArea / reference.wound2DApicalArea;
+            cellShorteningT30(numFile) = reference.apicalIndentionAvg - currentWoundArea.apicalIndentionAvg;
+
+            load(strrep(allSetMat(numFile).folder, 'Analysis', 'set.mat'))
+            paramsPerFile(numFile, :) = [Set.lambdaV, Set.lambdaV_Debris, Set.lambdaS1, Set.lambdaS2, Set.lambdaS3, Set.LambdaSFactor_Debris, Set.mu_bulk, Set.lambda_bulk, Set.cLineTension, Cell.EdgeLengths0_average, Cell.EdgeLengths0_lateralAverage, Set.kSubstrate, Set.TotalCells, max(Set.cellsToAblate), numFile];
+        end
+
+        woundData = table(maxRecoilingT6(:), apicalWoundAreaT30(:), cellShorteningT30(:));
+        woundData(end+1, :) = {1.65 0.52 0.023};
+        woundData.Properties.VariableNames = {'maxRecoilingT6', 'apicalWoundAreaT30', 'cellShorteningT30'};
+        woundData.error = ((woundData.maxRecoilingT6 - 1.63)).^2 + ((woundData.apicalWoundAreaT30 - 0.52)).^2;
+        woundData.difference = abs(woundData.maxRecoilingT6 - woundData.apicalWoundAreaT30);
+        %heatmap(woundData, 'cellShorteningT30', 'apicalWoundAreaT30', 'ColorVariable', 'maxRecoilingT6');
+        %% Regression
+        paramsPerFile = array2table(paramsPerFile, 'VariableNames', {'lambdaV', 'lambdaV_Debris', 'lambdaS1', 'lambdaS2', 'lambdaS3', 'LambdaSFactor_Debris', 'mu_bulk', 'lambda_bulk', 'LineTension', 'l_0_ApicoBasal', 'l_0_Lateral', 'Substrate', 'NumberOfCells', 'ablatedCells', 'id'});
+        fitglm([paramsPerFile, woundData(1:end-1, 1)])
+        fitglm([paramsPerFile, woundData(1:end-1, 2)])
+        fitglm([paramsPerFile, woundData(1:end-1, 3)])
     end
-    
-    woundData = table(maxRecoilingT6(:), apicalWoundAreaT30(:), cellShorteningT30(:));
-    woundData(end+1, :) = {1.65 0.52 0.023};
-    woundData.Properties.VariableNames = {'maxRecoilingT6', 'apicalWoundAreaT30', 'cellShorteningT30'};
-    woundData.error = ((woundData.maxRecoilingT6 - 1.63)).^2 + ((woundData.apicalWoundAreaT30 - 0.52)).^2;
-    woundData.difference = abs(woundData.maxRecoilingT6 - woundData.apicalWoundAreaT30);
-    %heatmap(woundData, 'cellShorteningT30', 'apicalWoundAreaT30', 'ColorVariable', 'maxRecoilingT6');
-    %% Regression
-    paramsPerFile = array2table(paramsPerFile, 'VariableNames', {'lambdaV', 'lambdaV_Debris', 'lambdaS1', 'lambdaS2', 'lambdaS3', 'LambdaSFactor_Debris', 'mu_bulk', 'lambda_bulk', 'LineTension', 'l_0_ApicoBasal', 'l_0_Lateral', 'Substrate', 'NumberOfCells', 'ablatedCells', 'id'});
-    fitglm([paramsPerFile, woundData(1:end-1, 1)])
-    fitglm([paramsPerFile, woundData(1:end-1, 2)])
-    fitglm([paramsPerFile, woundData(1:end-1, 3)])
 end

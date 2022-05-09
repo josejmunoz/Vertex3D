8fe3803b53eec76bd1e4c358d0a95658850f3556
Pablo <pablovm1990@gmail.com>
2022-05-09 14:21:46 +0100

    Ablating cells correctly

--- Src/Utilities/ablateCells.m
+++ Src/Utilities/ablateCells.m
@@ -2,15 +2,15 @@ function [Geo, Set] = ablateCells(Geo, Set, t)
 %PERFORMABLATION Summary of this function goes here
 %   Detailed explanation goes here
 if Set.Ablation == true && Set.TInitAblation <= t
-    if isempty(Set.cellsToAblate) == 0
+    if isempty(Geo.cellsToAblate) == 0
         disp('---- Performing ablation');
-        debrisCells = ismember(find(cellfun(@isempty, {Geo.Cells.AliveStatus})==0), Set.cellsToAblate); %Improve!!!
-        Geo.Cells(debrisCells).debris = true;
-        Geo.Cells(debrisCells).ExternalLambda = Set.lambdaSFactor_Debris;
-        Geo.Cells(debrisCells).InternalLambda = Set.lambdaSFactor_Debris;
-        Geo.Cells(debrisCells).SubstrateLambda = Set.lambdaSFactor_Debris;
-        
-        Set.cellsToAblate = [];
+        for debrisCell = Geo.cellsToAblate
+            Geo.Cells(debrisCell).AliveStatus = 0;
+            Geo.Cells(debrisCell).ExternalLambda = Set.lambdaSFactor_Debris;
+            Geo.Cells(debrisCell).InternalLambda = Set.lambdaSFactor_Debris;
+            Geo.Cells(debrisCell).SubstrateLambda = Set.lambdaSFactor_Debris;
+        end
+        Geo.cellsToAblate = [];
     end
 end
 end

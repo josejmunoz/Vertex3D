6e9bc3c4c17c21508707ff41e371faca07c4848a
Pablo <pablovm1990@gmail.com>
2022-05-16 09:51:32 +0100

    Bugfix

--- Src/Geo/Rebuild.m
+++ Src/Geo/Rebuild.m
@@ -23,7 +23,7 @@ function Geo = Rebuild(Geo, Set)
 				Geo.Cells(cc).Faces(j).Centre = sum(Geo.Cells(cc).Y(face_ids,:),1)/sum(face_ids);
 			else
 				% TODO FIXME, I think this is an unnecessary call most of the time...
-				Geo.Cells(cc).Faces(j).Tris	= BuildEdges(Geo.Cells(cc).T, face_ids, Geo.Cells(cc).Faces(j).Centre, Geo.Cells(cc).X, Geo.Cells(cc).Y);
+				Geo.Cells(cc).Faces(j).Tris	= BuildEdges(Geo.Cells(cc).T, face_ids, Geo.Cells(cc).Faces(j).Centre, Geo.Cells(cc).X, Geo.Cells(cc).Y, 1:Geo.nCells);
             	Geo.Cells(cc).Faces(j).InterfaceType	= BuildInterfaceType(ij, Geo.XgID);
                 Geo.Cells(cc).Faces(j).ij = ij;
 				[Geo.Cells(cc).Faces(j).Area, Geo.Cells(cc).Faces(j).TrisArea] = ComputeFaceArea(Geo.Cells(cc).Faces(j), Geo.Cells(cc).Y);	

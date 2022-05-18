16fe8de8781f61923ea7356e61744423d92096e8
Pablo <pablovm1990@gmail.com>
2022-05-18 14:13:14 +0100

    Input updated

--- Src/Input/Substrate.m
+++ Src/Input/Substrate.m
@@ -1,15 +1,23 @@
 Geo.nx = 3; 
 Geo.ny = 3; 
+Geo.nz = 1;
+
+Geo.cellsToAblate = [5];
  
-Set.lambdaV = 5; 
+Set.lambdaV = 10; 
  
 Set.tend=300; 
 Set.Nincr=300; 
  
-Set.lambdaS1 = 1; 
-Set.lambdaS2 = 0.5;  
+Set.lambdaS1 = 0.2; 
+Set.lambdaS2 = 0.2;  
  
-Set.Substrate  = true; 
+Set.Substrate  = 0; 
 Set.SubstrateZ = -0.5;
+
+Set.Contractility = true;
+Set.cLineTension = 0.001;
+
+%Set.Remodelling = 0;
  
 Set.OutputFolder='Result/Substrate';
--- Src/Set/WoundDefault.m
+++ Src/Set/WoundDefault.m
@@ -1,16 +1,13 @@
 function DSet = WoundDefault(DSet)
 	%% ============================== Ablation ============================
-	DSet.Ablation					= false;
+	DSet.Ablation					= true;
 	DSet.TInitAblation				= 1;
 	DSet.TEndAblation				= DSet.tend;
-	DSet.cellsToAblate				= 1;
 	DSet.lambdaSFactor_Debris		= 0.001;
 	%% =========================== Contractility ==========================
 	DSet.Contractility								 = 0;
-	DSet.cPurseString								 = 0;
-	DSet.Contractility_Variability_PurseString		 = ([1 1 2.5 2.5] - 1) * DSet.cPurseString;
+	DSet.Contractility_Variability_PurseString		 = ([1 1 2.5 2.5] - 1) * DSet.cLineTension;
 	DSet.Contractility_TimeVariability_PurseString	 = [0 5 16 60]/60*(DSet.TEndAblation - DSet.TInitAblation);
-	DSet.cLateralCables								 = 0;
-    DSet.Contractility_Variability_LateralCables	 = ([0.5 0.5 1.4 1.4] - 0.5) * DSet.cLateralCables; 
+    DSet.Contractility_Variability_LateralCables	 = ([0.5 0.5 1.4 1.4] - 0.5) * DSet.cLineTension; 
     DSet.Contractility_TimeVariability_LateralCables = [0 1 16 60]/60*(DSet.TEndAblation - DSet.TInitAblation); 
 end

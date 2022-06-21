40f2eed556163b2e891c07fb499b556d91c9f658
Pablo <pablovm1990@gmail.com>
2022-06-21 10:25:09 +0100

    Fix 'bad programming' and keep only good faces

--- Src/Geo/Rebuild.m
+++ Src/Geo/Rebuild.m
@@ -8,15 +8,9 @@ function Geo = Rebuild(Geo, Set)
 	        cj    = Neigh_nodes(j);
             ij			= [cc, cj];
             face_ids	= sum(ismember(Cell.T,ij),2)==2;
-			% TODO FIXME BAD PROGRAMMING...
-			newFace = true;
-			for jj = 1:length(Geo.Cells(cc).Faces)
-				Face = Geo.Cells(cc).Faces(jj);
-				if ismember(cj, Face.ij)
-					newFace = false;
-					break
-				end
-			end
+            
+			newFace = ~ismember(cj, [Geo.Cells(cc).Faces.ij]);
+            
 			if newFace
 				Geo.Cells(cc).Faces(j+1:length(Geo.Cells(cc).Faces)+1)=Geo.Cells(cc).Faces(j:length(Geo.Cells(cc).Faces));
 				Geo.Cells(cc).Faces(j)=BuildFace(cc, cj, Geo.nCells, Geo.Cells(cc), Geo.XgID, Set, Geo.XgTop, Geo.XgBottom);
@@ -24,11 +18,13 @@ function Geo = Rebuild(Geo, Set)
 			else
 				% TODO FIXME, I think this is an unnecessary call most of the time...
                 [Geo.Cells(cc).Faces(j).Tris] = BuildEdges(Geo.Cells(cc).T, face_ids, Geo.Cells(cc).Faces(j).Centre, Geo.Cells(cc).Faces(j).InterfaceType, Geo.Cells(cc).X, Geo.Cells(cc).Y, 1:Geo.nCells);
-            	Geo.Cells(cc).Faces(j).InterfaceType	= BuildInterfaceType(ij, Geo.XgID, Geo.XgTop, Geo.XgBottom);
+            	Geo.Cells(cc).Faces(j).InterfaceType = BuildInterfaceType(ij, Geo.XgID, Geo.XgTop, Geo.XgBottom);
                 Geo.Cells(cc).Faces(j).ij = ij;
 				[Geo.Cells(cc).Faces(j).Area] = ComputeFaceArea(vertcat(Geo.Cells(cc).Faces(j).Tris.Edge), Geo.Cells(cc).Y, Geo.Cells(cc).Faces(j).Centre);	
-			end
-		end
+            end
+            
+        end
+        Geo.Cells(cc).Faces = Geo.Cells(cc).Faces(1:length(Neigh_nodes));
 		Geo.Cells(cc).Area  = ComputeCellArea(Geo.Cells(cc));
         Geo.Cells(cc).Vol   = ComputeCellVolume(Geo.Cells(cc));
 	end

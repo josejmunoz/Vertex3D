236cb7fe1527f863a9c0ed4dd3f31140811d9295
Pablo Vicente Munuera <pablovm1990@gmail.com>
2022-07-21 09:19:54 +0100

    Working as expected, but not good

--- Src/Remodel/Flip24.m
+++ Src/Remodel/Flip24.m
@@ -1,4 +1,4 @@
-function [Geo_n, Geo, Dofs, Set, newYgIds] = Flip03(Geo_0, Geo_n, Geo, Dofs, Set, newYgIds)
+function [Geo_n, Geo, Dofs, Set, newYgIds] = Flip24(Geo_0, Geo_n, Geo, Dofs, Set, newYgIds)
 %FLIP03 Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -163,50 +163,19 @@ for c = 1:Geo.nCells
                     commonNodes(commonNodes == Face.ij(2)) = [];
                 end
                 
-                % Node to expand:
-                % TODO: NEED TO FIND A WAY OF SELECTING 1 NODE FROM THIS
-                % TUPLE
-                nodeToExpand = commonNodes(1);
-                newNodeIDs = [nodeToExpand length(Geo.Cells)+1];
-                connectedToNodeToExpand = commonNodes(commonNodes~=nodeToExpand);
+                newNodeIDs = length(Geo.Cells)+1;
                 
-                
-                % We will use the common nodes to create the two new nodes
-                % from the old one and perpendicular to the edge created by
-                % the commonNodes
-                A = Geo.Cells(nodeToExpand).X;
-                B = Geo.Cells(connectedToNodeToExpand).X;
-                C = Geo.Cells(opposingNodes(1)).X;
-                
-                % Regarding the cell centre
-                % https://stackoverflow.com/questions/28994044/find-a-point-on-a-line-perpendicular-and-through-the-middle-of-another-line/28994344#28994344
-                O = Geo.Cells(mainNode).X;
-                normalize = @(X) X/norm(X);
-                normalVector = normalize(cross(B-A, C-A));
-                perpendicularVector = cross(normalVector, B-A);
-                unitVector = normalize(perpendicularVector);
-                
-                % Get two nodes based on the perpendicular from the node to
-                % be splitted at a quarter of the distance
-                % (avgEdgesToFaceCentre).
-                % https://math.stackexchange.com/questions/175896/finding-a-point-along-a-line-a-certain-distance-away-from-another-point
-                newNode1 = A + unitVector*(avgEdgesToFaceCentre*2);
-                newNode2 = A - unitVector*(avgEdgesToFaceCentre*2);
-                
-                
-                Geo.Cells(nodeToExpand).X = newNode1;
-                Geo.Cells(newNodeIDs(2)).X = newNode2;
-                Geo_n.Cells(nodeToExpand).X = newNode1;
-                Geo_n.Cells(newNodeIDs(2)).X = newNode2;
+                Geo.Cells(newNodeIDs).X = mean(vertcat(Geo.Cells(commonNodes).X));
+                Geo_n.Cells(newNodeIDs).X = mean(vertcat(Geo_n.Cells(commonNodes).X));
                 
                 %TODO: ADD ALSO TO BOTTOM OR TOP
-                Geo.XgID(end+1) = newNodeIDs(2);
+                Geo.XgID(end+1) = newNodeIDs;
+                Geo_n.XgID(end+1) = newNodeIDs;
                 
                 %% Assign nodes to tets
-                originalTets = Geo.Cells(nodeToExpand).T;
-                oldTets = Geo.Cells(nodeToExpand).T;
+                oldTets = tetsToExpand;
                 
-                nodesToChange = [unique(originalTets); newNodeIDs(2)]; 
+                nodesToChange = [unique(oldTets); newNodeIDs]; 
                 newTets = nodesToChange(delaunayn(vertcat(Geo.Cells(nodesToChange).X)));
                 
                 % Remove tets with all Ghost Nodes
@@ -215,7 +184,6 @@ for c = 1:Geo.nCells
                 %newTets_removedNotInvolved = newTets(any(ismember(newTets, newNodeIDs), 2), :);
                 tetsToExclude_Possibly = newTets(~any(ismember(newTets, newNodeIDs), 2), :);
                 newTets_removedNotInvolved = newTets(any(ismember(newTets, newNodeIDs), 2), :);
-                
                 addOrNot = [];
                 for newTet = tetsToExclude_Possibly'
                     if mod(sum(sum(ismember(newTets_removedNotInvolved, newTet), 2) > 2), 2)
@@ -225,29 +193,15 @@ for c = 1:Geo.nCells
                     end
                 end
                 
-                newTets_removedNotInvolved = [newTets_removedNotInvolved; tetsToExclude_Possibly(addOrNot, :)];
+                newTets = [newTets_removedNotInvolved; tetsToExclude_Possibly(addOrNot==1, :)];
                 
-%                 numNewTetToRemove = [];
-%                 for numTet = 1:size(newTets, 1)
-%                     currentTet = newTets(numTet, :);
-%                     currentTet(currentTet == newNodeIDs(2)) = [];
-%                     for numCell = currentTet
-%                         if ~isempty(Geo.Cells(numCell).AliveStatus) && ~all(ismember(currentTet, Geo.Cells(numCell).T))
-%                             numNewTetToRemove(end+1) = numTet;
-%                             currentTet;
-%                             break;
-%                         end
-%                     end
-%                 end
-                
-                newTets_removedNotInvolved(numNewTetToRemove, :) = [];
                 
                 [Geo] = RemoveTetrahedra(Geo, oldTets);
                 [Geo_n] = RemoveTetrahedra(Geo_n, oldTets);
-                [Geo] = AddTetrahedra(Geo, newTets_removedNotInvolved, Set);
-                [Geo_n] = AddTetrahedra(Geo_n, newTets_removedNotInvolved, Set);
+                [Geo] = AddTetrahedra(Geo, newTets, Set);
+                [Geo_n] = AddTetrahedra(Geo_n, newTets, Set);
                 
-                %visualizeTets(Geo_n.Cells(1).T, Geo_n)
+                visualizeTets(Geo_n.Cells(3).T, Geo_n)
                 
                 %% All this, goes together when remodel occurs. TODO: PUT TOGETHER AS A FUNCTION
                 Geo   = Rebuild(Geo, Set);
@@ -263,7 +217,7 @@ for c = 1:Geo.nCells
                 %targetTets = testToSubstitute;
                 if CheckTris(Geo) %%&& ~CheckConvexity(Tnew,Geo_backup)
                     Dofs = GetDOFs(Geo, Set);
-                    [Dofs, Geo]  = GetRemodelDOFs(newTets_removedNotInvolved, Dofs, Geo);
+                    [Dofs, Geo]  = GetRemodelDOFs(newTets, Dofs, Geo);
                     [Geo, Set, DidNotConverge] = SolveRemodelingStep(Geo_0, Geo_n, Geo, Dofs, Set);
                     if DidNotConverge
                         Geo   = Geo_backup;

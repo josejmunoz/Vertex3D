93869c92daad90c7880e30f3295190b82c6fc49b
Pablo Vicente Munuera <pablovm1990@gmail.com>
2022-07-25 11:30:14 +0100

    When 3 neighbours each, calculate which vertex expand

--- Src/Remodel/Flip13.m
+++ Src/Remodel/Flip13.m
@@ -2,12 +2,15 @@ function [Geo_n, Geo, Dofs, Set, newYgIds] = Flip13(Geo_0, Geo_n, Geo, Dofs, Set
 %FLIP13 Summary of this function goes here
 %   Detailed explanation goes here
 
+allFaces = [Geo.Cells.Faces];
+allTris = [allFaces.Tris];
+allEdges = vertcat(allTris.Edge);
 for c = 1:Geo.nCells
     
     f = 0;
-    allTris = [Geo.Cells(c).Faces.Tris];
-    avgArea = mean([allTris.Area]);
-    stdArea = std([allTris.Area]);
+    allTrisCurrentCell = [Geo.Cells(c).Faces.Tris];
+    avgArea = mean([allTrisCurrentCell.Area]);
+    stdArea = std([allTrisCurrentCell.Area]);
     
     %CARE: Number of faces change within this loop, so it should be a while
     while f < length(Geo.Cells(c).Faces)
@@ -31,8 +34,13 @@ for c = 1:Geo.nCells
         [idNeighbours_1, numNeighbours_1] = getVertexNeighbours(Geo, trisToChange.Edge(1), c);
         [idNeighbours_2, numNeighbours_2] = getVertexNeighbours(Geo, trisToChange.Edge(2), c);
         
-        if numNeighbours_1 == 3 || numNeighbours_2 == 3
+        if numNeighbours_1 == 3 && numNeighbours_2 == 3
+            [trisArea_1, trisEdges_1] = getTrisAreaOfNeighbours(Geo, Geo.Cells(c).globalIds(trisToChange.Edge(1)));
+            [trisArea_2, trisEdges_2] = getTrisAreaOfNeighbours(Geo, Geo.Cells(c).globalIds(trisToChange.Edge(2)));
+        elseif numNeighbours_1 == 3
             idNeighbours_1
+        elseif numNeighbours_2 == 3
+            idNeighbours_2
         end
     end
 end

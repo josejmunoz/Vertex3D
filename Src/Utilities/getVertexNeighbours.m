7b7889f41d589a8fcec297370abdf5b0398aa9ff
Pablo Vicente Munuera <pablovm1990@gmail.com>
2022-07-25 12:00:08 +0100

    Get tets of vertex neighbours

--- Src/Utilities/getVertexNeighbours.m
+++ Src/Utilities/getVertexNeighbours.m
@@ -1,4 +1,4 @@
-function [idNeighbours, numNeighbours] = getVertexNeighbours(Geo, idVertex, idCell)
+function [idNeighbours, numNeighbours, tetsNeighbours] = getVertexNeighbours(Geo, idVertex, idCell)
 %GETVERTEXNEIGHBOURS Get number and ID of neighbours of a vertex
 %   Two tets are neighbours if they share a face.
 %   Vertex and Tetrahedra share the same ID
@@ -10,6 +10,7 @@ idNeighbours = find(sum(ismember(allTets, Geo.Cells(idCell).T(idVertex, :)), 2) 
 [~, uniqueIDs] = unique(sort(allTets(idNeighbours, :), 2), 'rows');
 idNeighbours = idNeighbours(uniqueIDs);
 numNeighbours = length(idNeighbours);
+tetsNeighbours = allTets(idNeighbours(uniqueIDs), :);
 
 end
 

62a4be55757e13fa8a0c3423e4dab59810839570
Pablo Vicente Munuera <pablovm1990@gmail.com>
2022-07-27 16:54:18 +0100

    Change with no assumptions

--- Src/DOFs/GetRemodelDOFs.m
+++ Src/DOFs/GetRemodelDOFs.m
@@ -1,17 +1,17 @@
 function [Dofs, Geo] = GetRemodelDOFs(Tnew, Dofs, Geo)
-	remodelDofs = zeros(0,1);
-	for ccc = 1:Geo.nCells
-		news = find(sum(ismember(Tnew,ccc)==1,2));
-		remodelDofs(end+1:end+length(news),:) = Geo.Cells(ccc).globalIds(end-length(news)+1:end,:);
-		for jj = 1:length(Geo.Cells(ccc).Faces)
-			Face_r = Geo.Cells(ccc).Faces(jj);
-			FaceTets = Geo.Cells(ccc).T(unique([Face_r.Tris.Edge]),:);
-			if any(ismember(sort(FaceTets, 2),sort(Tnew, 2), 'rows'))
-				remodelDofs(end+1,:) = Face_r.globalIds;
-            end
-		end
-	end
-	Dofs.Remodel = unique(remodelDofs, 'rows');
-	Dofs.Remodel = 3.*(kron(Dofs.Remodel',[1 1 1])-1)+kron(ones(1,length(Dofs.Remodel')),[1 2 3]);
-	Geo.AssemblegIds  = unique(unique(remodelDofs, 'rows'));
+remodelDofs = zeros(0,1);
+for numCell = 1:Geo.nCells
+    news = ismember(sort(Geo.Cells(numCell).T, 2), sort(Tnew, 2), 'rows');
+    remodelDofs(end+1:end+sum(news),:) = Geo.Cells(numCell).globalIds(news);
+    for jj = 1:length(Geo.Cells(numCell).Faces)
+        Face_r = Geo.Cells(numCell).Faces(jj);
+        FaceTets = Geo.Cells(numCell).T(unique([Face_r.Tris.Edge]),:);
+        if any(ismember(sort(FaceTets, 2),sort(Tnew, 2), 'rows'))
+            remodelDofs(end+1,:) = Face_r.globalIds;
+        end
+    end
+end
+Dofs.Remodel = unique(remodelDofs, 'rows');
+Dofs.Remodel = 3.*(kron(Dofs.Remodel',[1 1 1])-1)+kron(ones(1,length(Dofs.Remodel')),[1 2 3]);
+Geo.AssemblegIds  = unique(unique(remodelDofs, 'rows'));
 end

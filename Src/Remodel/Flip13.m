96f57b7ae6774747fb2d5adcc0026e24bb7c2b05
Pablo Vicente Munuera <pablovm1990@gmail.com>
2022-07-27 16:21:03 +0100

    Trying to make it general, still nothing

--- Src/Remodel/Flip13.m
+++ Src/Remodel/Flip13.m
@@ -7,8 +7,7 @@ hasConverged = 0;
 [~, numNeighbours_1, tetsNeighbours_1] = getVertexNeighbours(Geo, trisToChange.Edge(1), numCell);
 [~, numNeighbours_2, tetsNeighbours_2] = getVertexNeighbours(Geo, trisToChange.Edge(2), numCell);
 
-vertexToExpand = -1;
-if numNeighbours_1 == 3 && numNeighbours_2 == 3
+if numNeighbours_1 == numNeighbours_2
     [trisArea_1, ~] = getTrisAreaOfNeighbours(Geo, Geo.Cells(numCell).globalIds(trisToChange.Edge(1)));
     [trisArea_2, ~] = getTrisAreaOfNeighbours(Geo, Geo.Cells(numCell).globalIds(trisToChange.Edge(2)));
     
@@ -19,64 +18,69 @@ if numNeighbours_1 == 3 && numNeighbours_2 == 3
         vertexToExpand = trisToChange.Edge(2);
         oldTets = Geo.Cells(numCell).T(trisToChange.Edge(2), :);
     end
-elseif numNeighbours_1 == 3
+elseif numNeighbours_1 < numNeighbours_2
     vertexToExpand = trisToChange.Edge(1);
     oldTets = Geo.Cells(numCell).T(trisToChange.Edge(1), :);
-elseif numNeighbours_2 == 3
+elseif numNeighbours_1 > numNeighbours_2
     vertexToExpand = trisToChange.Edge(2);
     oldTets = Geo.Cells(numCell).T(trisToChange.Edge(2), :);
 end
 
-if vertexToExpand ~= -1
-    fprintf('=>> 13 Flip.\n');
-    nodesWithoutTheCell = Geo.Cells(numCell).T(vertexToExpand, :);
-    nodesWithoutTheCell(~cellfun(@isempty, {Geo.Cells(nodesWithoutTheCell).AliveStatus})) = [];
-    allNodesPos = mean(vertcat(Geo.Cells(Geo.Cells(numCell).T(vertexToExpand, :)).X));
-    nodesWithoutCellPos = mean(vertcat(Geo.Cells(nodesWithoutTheCell).X));
-    newNodePosition = [allNodesPos(1:2), nodesWithoutCellPos(3)];
-    
-    [Geo, newNodeIDs] = AddNewNode(Geo, newNodePosition);
-    [Geo_n] = AddNewNode(Geo_n, newNodePosition);
-    
-    [newTets] = DoFlip13(Geo, newNodeIDs, oldTets');
-    
-    [Geo] = RemoveTetrahedra(Geo, oldTets);
-    [Geo_n] = RemoveTetrahedra(Geo_n, oldTets);
-    [Geo] = AddTetrahedra(Geo, newTets, Set);
-    [Geo_n] = AddTetrahedra(Geo_n, newTets, Set);
-    
-    %% All this, goes together when remodel occurs. TODO: PUT TOGETHER AS A FUNCTION
-    Geo   = Rebuild(Geo, Set);
-    Geo_n = Rebuild(Geo_n, Set);
-    
-    Geo   = BuildGlobalIds(Geo);
-    Geo_n = BuildGlobalIds(Geo_n);
-    
-    Geo   = UpdateMeasures(Geo);
-    Geo_n = UpdateMeasures(Geo_n);
-    
-    if CheckTris(Geo) %%&& ~CheckConvexity(Tnew,Geo_backup)
-        Dofs = GetDOFs(Geo, Set);
-        [Dofs, Geo]  = GetRemodelDOFs(newTets, Dofs, Geo);
-        [Geo, Set, DidNotConverge] = SolveRemodelingStep(Geo_0, Geo_n, Geo, Dofs, Set);
-        if DidNotConverge
-            Geo   = Geo_backup;
-            Geo_n = Geo_n_backup;
-            fprintf('=>> 13-Flip rejected: did not converge\n');
-            return
-        end
-        
-        newYgIds = unique([newYgIds; Geo.AssemblegIds]);
-        Geo   = UpdateMeasures(Geo);
-        Geo_n = UpdateMeasures(Geo_n);
-        PostProcessingVTK(Geo, Geo_0, Set, Set.iIncr+2)
-        hasConverged = 1;
-    else
+fprintf('=>> 13 Flip.\n');
+nodesWithoutTheCell = Geo.Cells(numCell).T(vertexToExpand, :);
+nodesWithoutTheCell(~cellfun(@isempty, {Geo.Cells(nodesWithoutTheCell).AliveStatus})) = [];
+allNodesPos = mean(vertcat(Geo.Cells(Geo.Cells(numCell).T(vertexToExpand, :)).X));
+nodesWithoutCellPos = mean(vertcat(Geo.Cells(nodesWithoutTheCell).X));
+newNodePosition = [allNodesPos(1:2), nodesWithoutCellPos(3)];
+
+[Geo, newNodeIDs] = AddNewNode(Geo, newNodePosition);
+[Geo_n] = AddNewNode(Geo_n, newNodePosition);
+
+[newTets] = DoFlip13(Geo, newNodeIDs, oldTets');
+
+if isempty(newTets)
+    Geo   = Geo_backup;
+    Geo_n = Geo_n_backup;
+    fprintf('=>> 13-Flip rejected: is not compatible\n');
+    return
+end
+
+[Geo] = RemoveTetrahedra(Geo, oldTets);
+[Geo_n] = RemoveTetrahedra(Geo_n, oldTets);
+[Geo] = AddTetrahedra(Geo, newTets, Set);
+[Geo_n] = AddTetrahedra(Geo_n, newTets, Set);
+
+%% All this, goes together when remodel occurs. TODO: PUT TOGETHER AS A FUNCTION
+Geo   = Rebuild(Geo, Set);
+Geo_n = Rebuild(Geo_n, Set);
+
+Geo   = BuildGlobalIds(Geo);
+Geo_n = BuildGlobalIds(Geo_n);
+
+Geo   = UpdateMeasures(Geo);
+Geo_n = UpdateMeasures(Geo_n);
+
+if CheckTris(Geo) %%&& ~CheckConvexity(Tnew,Geo_backup)
+    Dofs = GetDOFs(Geo, Set);
+    [Dofs, Geo]  = GetRemodelDOFs(newTets, Dofs, Geo);
+    [Geo, Set, DidNotConverge] = SolveRemodelingStep(Geo_0, Geo_n, Geo, Dofs, Set);
+    if DidNotConverge
         Geo   = Geo_backup;
         Geo_n = Geo_n_backup;
-        fprintf('=>> 13-Flip rejected: is not compatible\n');
+        fprintf('=>> 13-Flip rejected: did not converge\n');
         return
     end
+    
+    newYgIds = unique([newYgIds; Geo.AssemblegIds]);
+    Geo   = UpdateMeasures(Geo);
+    Geo_n = UpdateMeasures(Geo_n);
+    PostProcessingVTK(Geo, Geo_0, Set, Set.iIncr+2)
+    hasConverged = 1;
+else
+    Geo   = Geo_backup;
+    Geo_n = Geo_n_backup;
+    fprintf('=>> 13-Flip rejected: is not compatible\n');
+    return
 end
 end
 

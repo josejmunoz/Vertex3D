9c0678652523d5662eeef37c729086569310f3d3
Pablo Vicente Munuera <pablovm1990@gmail.com>
2022-07-27 16:21:45 +0100

    Error for DoFlip only fprintf now

--- Src/Remodel/DoFlip13.m
+++ Src/Remodel/DoFlip13.m
@@ -2,16 +2,17 @@ function [newTets] = DoFlip13(Geo, newNodeIDs, oldTets)
 %DOFLIP13 Summary of this function goes here
 %   Detailed explanation goes here
 
+newTets = [];
 mainNode = oldTets(~cellfun(@isempty, {Geo.Cells(oldTets).AliveStatus}));
 remainingNodes = setdiff(oldTets, mainNode);
 
 if length(mainNode) > 1
-    error('DoFlip13 more than 1 mainNode');
+    fprintf('DoFlip13 more than 1 mainNode\n');
+    return
 end
 
 combinationsToCreate = nchoosek(remainingNodes, 2);
 
-newTets = [];
 for combination = combinationsToCreate'
     newTets(end+1, 1:4) = [newNodeIDs, mainNode, combination'];
 end

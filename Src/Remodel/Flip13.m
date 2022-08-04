d95d4891b4b34872df4f91251495ead63dea20bf
Pablo Vicente Munuera <pablovm1990@gmail.com>
2022-08-04 09:16:23 +0100

    Not sure about this (double check)

--- Src/Remodel/Flip13.m
+++ Src/Remodel/Flip13.m
@@ -7,21 +7,26 @@ hasConverged = 0;
 [~, numNeighbours_1, tetsNeighbours_1] = getVertexNeighbours(Geo, trisToChange.Edge(1), numCell);
 [~, numNeighbours_2, tetsNeighbours_2] = getVertexNeighbours(Geo, trisToChange.Edge(2), numCell);
 
-if numNeighbours_1 == numNeighbours_2
-    [trisArea_1, ~] = getTrisAreaOfNeighbours(Geo, Geo.Cells(numCell).globalIds(trisToChange.Edge(1)));
-    [trisArea_2, ~] = getTrisAreaOfNeighbours(Geo, Geo.Cells(numCell).globalIds(trisToChange.Edge(2)));
+[trisArea_1, ~] = getTrisAreaOfNeighbours(Geo, Geo.Cells(numCell).globalIds(trisToChange.Edge(1)));
+[trisArea_2, ~] = getTrisAreaOfNeighbours(Geo, Geo.Cells(numCell).globalIds(trisToChange.Edge(2)));
+
+if ~all(trisArea_1 > Set.upperAreaThreshold)
+    trisArea_1 = 0;
+end
+
+if ~all(trisArea_2 > Set.upperAreaThreshold)
+    trisArea_2 = 0;
+end
     
-    if sum(trisArea_1) > sum(trisArea_2)
-        vertexToExpand = trisToChange.Edge(1);
-        oldTets = Geo.Cells(numCell).T(trisToChange.Edge(1), :);
-    else
-        vertexToExpand = trisToChange.Edge(2);
-        oldTets = Geo.Cells(numCell).T(trisToChange.Edge(2), :);
-    end
-elseif numNeighbours_1 < numNeighbours_2
+if isequal(trisArea_2, 0) && isequal(trisArea_1, 0)
+    %fprintf('=>> 13-Flip rejected: not big enough triangles\n');
+    return
+end
+
+if sum(trisArea_1 > Set.upperAreaThreshold) > sum(trisArea_2 > Set.upperAreaThreshold)
     vertexToExpand = trisToChange.Edge(1);
     oldTets = Geo.Cells(numCell).T(trisToChange.Edge(1), :);
-elseif numNeighbours_1 > numNeighbours_2
+else
     vertexToExpand = trisToChange.Edge(2);
     oldTets = Geo.Cells(numCell).T(trisToChange.Edge(2), :);
 end
@@ -29,16 +34,15 @@ end
 fprintf('=>> 13 Flip.\n');
 nodesWithoutTheCell = Geo.Cells(numCell).T(vertexToExpand, :);
 nodesWithoutTheCell(~cellfun(@isempty, {Geo.Cells(nodesWithoutTheCell).AliveStatus})) = [];
-allNodesPos = mean(vertcat(Geo.Cells(Geo.Cells(numCell).T(vertexToExpand, :)).X));
 nodesWithoutCellPos = mean(vertcat(Geo.Cells(nodesWithoutTheCell).X));
-newNodePosition = [allNodesPos(1:2), nodesWithoutCellPos(3)];
+newNodePosition = nodesWithoutCellPos;
 
 [Geo, newNodeIDs] = AddNewNode(Geo, newNodePosition);
 [Geo_n] = AddNewNode(Geo_n, newNodePosition);
 
-[newTets] = DoFlip13(Geo, newNodeIDs, oldTets');
+[Tnew] = DoFlip13(Geo, newNodeIDs, oldTets');
 
-if isempty(newTets)
+if isempty(Tnew) || CheckOverlappingTets(oldTets, Tnew, Geo)
     Geo   = Geo_backup;
     Geo_n = Geo_n_backup;
     fprintf('=>> 13-Flip rejected: is not compatible\n');
@@ -47,8 +51,8 @@ end
 
 [Geo] = RemoveTetrahedra(Geo, oldTets);
 [Geo_n] = RemoveTetrahedra(Geo_n, oldTets);
-[Geo] = AddTetrahedra(Geo, newTets, Set);
-[Geo_n] = AddTetrahedra(Geo_n, newTets, Set);
+[Geo] = AddTetrahedra(Geo, Tnew, Set);
+[Geo_n] = AddTetrahedra(Geo_n, Tnew, Set);
 
 %% All this, goes together when remodel occurs. TODO: PUT TOGETHER AS A FUNCTION
 Geo   = Rebuild(Geo, Set);
@@ -62,7 +66,7 @@ Geo_n = UpdateMeasures(Geo_n);
 
 if CheckTris(Geo) %%&& ~CheckConvexity(Tnew,Geo_backup)
     Dofs = GetDOFs(Geo, Set);
-    [Dofs, Geo]  = GetRemodelDOFs(newTets, Dofs, Geo);
+    [Dofs, Geo]  = GetRemodelDOFs(Tnew, Dofs, Geo);
     [Geo, Set, DidNotConverge] = SolveRemodelingStep(Geo_0, Geo_n, Geo, Dofs, Set);
     if DidNotConverge
         Geo   = Geo_backup;

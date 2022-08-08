ea5e90a5b5f2fbb6691da933c757d711064815b2
Pablo Vicente Munuera <pablovm1990@gmail.com>
2022-08-08 09:42:29 +0100

    Init KgTriangle Aspect Ratio energy barrier

--- Src/Geo/ComputeTriEnergy.m
+++ Src/Geo/ComputeTriEnergy.m
@@ -2,12 +2,16 @@
 function [nrgs]=ComputeTriEnergy(Face, Ys, Set)
     nrgs = zeros(0,1);
 	for t = 1:length(Face.Tris)
-        Tri = Face.Tris(t).Edge;
-        Y3 = Face.Centre;
-        
-        YTri = [Ys(Tri(1),:); Ys(Tri(2),:); Y3];
-        area = (1/2)*norm(cross(YTri(2,:)-YTri(1,:),YTri(1,:)-YTri(3,:)));
-        nrg  = exp(Set.lambdaB*(1-Set.Beta*area/Set.BarrierTri0));
+        %Tri = Face.Tris(t).Edge;
+        %Y3 = Face.Centre;
+        %YTri = [Ys(Tri(1),:); Ys(Tri(2),:); Y3];
+        %area = (1/2)*norm(cross(YTri(2,:)-YTri(1,:),YTri(1,:)-YTri(3,:)));
+        %nrg  = exp(Set.lambdaB*(1-Set.Beta*area/Set.BarrierTri0));
+        [sideLengths] = ComputeTriSideLengths(Face, t, Ys);
+        [aspectRatio] = ComputeTriAspectRatio(sideLengths);
+        nrg  = exp(Set.lambdaB*6*(1/(1-aspectRatio)));
+%         aspectRatio
+%         nrg
         nrgs(end+1) = nrg;
 	end
 end
---
+++ Src/Kg/KgTriAREnergyBarrier.m
@@ -0,0 +1,47 @@
+function [g,K,EnergyB]=KgTriAREnergyBarrier(Geo,Set)
+% The residual g and Jacobian K of  Energy Barrier
+% Energy  WBexp = exp( Set.lambdaB*  ( 1 - Set.Beta*At/Set.BarrierTri0 )  );
+
+	[g, K] = initializeKg(Geo, Set);
+	EnergyB = 0;
+	for c=1:Geo.nCells
+		if Geo.Remodelling
+			if ~ismember(c,Geo.AssembleNodes)
+        		continue
+			end
+        end
+        
+        if isempty(Geo.Cells(c).AliveStatus) || Geo.Cells(c).AliveStatus ~= 1
+            continue
+        end
+        
+		Cell = Geo.Cells(c);
+		Ys = Cell.Y;
+		lambdaB=Set.lambdaB;
+		for f = 1:length(Cell.Faces)
+            Face = Cell.Faces(f);
+			Tris = Cell.Faces(f).Tris;
+			for t = 1:length(Tris)
+				fact=-((lambdaB*Set.Beta)/Set.BarrierTri0)* ... % * ...
+				                    exp(lambdaB*(1-Set.Beta*Face.Tris(t).Area/Set.BarrierTri0));
+				fact2=fact*-((lambdaB*Set.Beta)/Set.BarrierTri0);
+				y1 = Ys(Tris(t).Edge(1),:);
+                y2 = Ys(Tris(t).Edge(2),:);
+                y3 = Cell.Faces(f).Centre;
+                n3 = Cell.Faces(f).globalIds;
+				nY = [Cell.globalIds(Tris(t).Edge)', n3];
+				if Geo.Remodelling
+					if ~any(ismember(nY,Geo.AssemblegIds))
+                        continue
+					end
+				end
+                [gs,Ks,Kss]=gKSArea(y1,y2,y3);
+	        	g=Assembleg(g,gs*fact,nY);	
+				Ks=(gs)*(gs')*fact2+Ks*fact+Kss*fact;
+				K= AssembleK(K,Ks,nY);
+				EnergyB=EnergyB+ exp(lambdaB*(1-Set.Beta*Face.Tris(t).Area/Set.BarrierTri0));
+%                 fprintf("%.12f %.12f %.12f %.3f %.3f %3f %d %d %d\n", norm(g), norm(K), EnergyB, y3, c, f, t);
+			end
+		end
+	end
+end

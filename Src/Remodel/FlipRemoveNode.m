43502ae3fef355ae3ff6a68b87ce499589295608
Pablo Vicente Munuera <pablovm1990@gmail.com>
2022-08-10 16:46:58 +0100

    intercalations with two or more cells working

--- Src/Remodel/FlipRemoveNode.m
+++ Src/Remodel/FlipRemoveNode.m
@@ -1,4 +1,4 @@
-function [Geo_n, Geo, Dofs, Set, newYgIds, hasConverged] = FlipRemoveNode(nodeToRemove, Geo_0, Geo_n, Geo, Dofs, Set, newYgIds)
+function [Geo_n, Geo, Dofs, Set, newYgIds, hasConverged] = FlipRemoveNode(nodeToRemove, Geo_0, Geo_n, Geo, Dofs, Set, newYgIds, internalFlip)
 %FLIPREMOVENODE Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,18 +6,27 @@ hasConverged = 0;
 
 oldTets = Geo.Cells(nodeToRemove).T;
 nodesToChange = getNodeNeighbours(Geo, nodeToRemove);
-mainNode = nodesToChange(~cellfun(@isempty, {Geo.Cells(nodesToChange).AliveStatus}));
+mainNodes = nodesToChange(~cellfun(@isempty, {Geo.Cells(nodesToChange).AliveStatus}));
 
-if length(mainNode) > 1
-    return
+Tnew = [];
+for mainNode = mainNodes'
+    currentOldTets = oldTets(any(ismember(oldTets, mainNode), 2), :);
+    currenteNodesToChange = unique(currentOldTets);
+    currenteNodesToChange(nodeToRemove == currenteNodesToChange) = [];
+    [c_Tnew] = ConnectTetrahedra(Geo, currenteNodesToChange, currentOldTets, mainNode);
+    Tnew = vertcat(Tnew, c_Tnew);
 end
-
-[Tnew] = ConnectTetrahedra(Geo, nodesToChange, oldTets, mainNode);
 
 %figure, tetramesh(Tnew, vertcat(Geo.Cells.X));
 %figure, tetramesh(oldTets, vertcat(Geo.Cells.X));
 
-[Geo, Geo_n, Dofs, newYgIds, hasConverged] = PostFlip(Tnew, oldTets, Geo, Geo_n, Geo_0, Dofs, newYgIds, Set, 'RemoveNode');
+if length(mainNodes) > 1
+    internalFlip = 1;
+else
+    internalFlip = 0;
+end
+    
+[Geo, Geo_n, Dofs, newYgIds, hasConverged] = PostFlip(Tnew, oldTets, Geo, Geo_n, Geo_0, Dofs, newYgIds, Set, 'RemoveNode', internalFlip);
 
 end
 
--- Src/Remodel/lib/PostFlip.m
+++ Src/Remodel/lib/PostFlip.m
@@ -1,11 +1,15 @@
-function [Geo, Geo_n, Dofs, newYgIds, hasConverged] = PostFlip(Tnew, oldTets, Geo, Geo_n, Geo_0, Dofs, newYgIds, Set, flipName)
+function [Geo, Geo_n, Dofs, newYgIds, hasConverged] = PostFlip(Tnew, oldTets, Geo, Geo_n, Geo_0, Dofs, newYgIds, Set, flipName, internalFlip)
 %POSTFLIP Summary of this function goes here
 %   Detailed explanation goes here
 
 hasConverged = 0;
 Geo_backup = Geo; Geo_n_backup = Geo_n; Dofs_backup = Dofs;
 
-if isempty(Tnew) || CheckOverlappingTets(oldTets, Tnew, Geo)
+if ~exist('internalFlip', 'var')
+    internalFlip = 0;
+end
+
+if isempty(Tnew) || CheckOverlappingTets(oldTets, Tnew, Geo, internalFlip)
     Geo   = Geo_backup;
     Geo_n = Geo_n_backup;
     fprintf('=>> %s-Flip rejected: is not compatible\n', flipName);

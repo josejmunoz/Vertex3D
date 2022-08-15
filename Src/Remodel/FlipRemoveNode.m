b13fb99ec2b79b2d7d100dfa59663150df78ad3e
Pablo Vicente Munuera <pablovm1990@gmail.com>
2022-08-15 15:57:25 +0100

    Bugfix

--- Src/Remodel/FlipRemoveNode.m
+++ Src/Remodel/FlipRemoveNode.m
@@ -1,4 +1,4 @@
-function [Geo_n, Geo, Dofs, Set, newYgIds, hasConverged] = FlipRemoveNode(nodeToRemove, Geo_0, Geo_n, Geo, Dofs, Set, newYgIds, flipName)
+function [Geo_n, Geo, Dofs, Set, newYgIds, hasConverged] = FlipRemoveNode(nodeToRemove, Geo_0, Geo_n, Geo, Dofs, Set, newYgIds)
 %FLIPREMOVENODE Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -7,6 +7,12 @@ hasConverged = 0;
 oldTets = Geo.Cells(nodeToRemove).T;
 nodesToChange = getNodeNeighbours(Geo, nodeToRemove);
 mainNodes = nodesToChange(~cellfun(@isempty, {Geo.Cells(nodesToChange).AliveStatus}));
+
+if length(mainNodes) > 1
+    flipName = 'RemoveNode: 2+ Main nodes';
+else
+    flipName = 'RemoveNode';
+end
 
 Tnew = [];
 for mainNode = mainNodes'
@@ -23,12 +29,6 @@ end
 
 %figure, tetramesh(Tnew, vertcat(Geo.Cells.X));
 %figure, tetramesh(oldTets, vertcat(Geo.Cells.X));
-
-if length(mainNodes) > 1
-    flipName = 'RemoveNode: 2+ Main nodes';
-else
-    flipName = 'RemoveNode';
-end
     
 [Geo, Geo_n, Dofs, newYgIds, hasConverged] = PostFlip(Tnew, oldTets, Geo, Geo_n, Geo_0, Dofs, newYgIds, Set, flipName);
 

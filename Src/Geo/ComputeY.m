71d3d701f288f5f9fed6dbaf84a2901638280be3
Pablo Vicente Munuera <pablovm1990@gmail.com>
2022-08-15 15:53:44 +0100

    Refactor

--- Src/Geo/ComputeY.m
+++ Src/Geo/ComputeY.m
@@ -3,14 +3,12 @@ function [newY] = ComputeY(x, cellCentre, lengthOfAlivedCells, Set)
 %   Detailed explanation goes here
     % Condition for the case where 3 nodes are ghost nodes,
     % i.e. external vertex
+    newY = mean(x);
     if lengthOfAlivedCells == 1
-        Center=(sum(x,1))/4;
-        vc=Center-cellCentre;
+        vc=newY-cellCentre;
         dir=vc/norm(vc);
         offset=Set.f*dir;
         newY = cellCentre+offset;
-    else
-        newY = mean(x);
     end
 end
 

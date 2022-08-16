9f3ef6fed947292e87bccf5c96df6b310aa596eb
Pablo Vicente Munuera <pablovm1990@gmail.com>
2022-08-16 09:16:56 +0100

    If cant decide, decide by distance

--- Src/Build/DecideXgTopOrBottomByNeigh.m
+++ Src/Build/DecideXgTopOrBottomByNeigh.m
@@ -2,16 +2,18 @@ function [location] = DecideXgTopOrBottomByNeigh(Geo, surroundingNodes, newNodeP
 %DECIDEXGTOPORBOTTOMBYNEIGH Summary of this function goes here
 %   Detailed explanation goes here
 
-percentageOfBottom = sum(ismember(surroundingNodes, Geo.XgBottom))/numel(surroundingNodes);
-percentageOfTop = sum(ismember(surroundingNodes, Geo.XgTop))/numel(surroundingNodes);
+bottomGhostNodes = ismember(surroundingNodes, Geo.XgBottom);
+topGhostNodes = ismember(surroundingNodes, Geo.XgTop);
+percentageOfBottom = sum(bottomGhostNodes)/numel(surroundingNodes);
+percentageOfTop = sum(topGhostNodes)/numel(surroundingNodes);
 
 if percentageOfBottom > 2*percentageOfTop
     location = 2; %% Bottom
 elseif 2*percentageOfBottom < percentageOfTop
     location = 1; %% Top
 else %% Need further tests
-    [~, idClosest] = pdist2(newNodePosition, vertcat(Geo.Cells(surroundingNodes).X), 'euclidean', 'smallest', 1);
-    if ismember(surroundingNodes(idClosest), Geo.XgTop)
+    [distances] = pdist2(vertcat(Geo.Cells(surroundingNodes).X), newNodePosition, 'euclidean');
+    if mean(distances(topGhostNodes)) < mean(distances(bottomGhostNodes))
         location = 1; %% Top
     else
         location = 2; %% Bottom

d51ecdb682fdc0c196bea790d7e1d2391469a5db
Pablo Vicente Munuera <pablovm1990@gmail.com>
2022-08-16 16:36:52 +0100

    AddTetrahedra computeY as in BuildYFromX

--- Src/Build/AddTetrahedra.m
+++ Src/Build/AddTetrahedra.m
@@ -6,16 +6,18 @@ for newTet = newTets'
         if isempty(Geo.Cells(numNode).T) || all(~ismember(sort(Geo.Cells(numNode).T, 2), sort(newTet', 2), 'rows'))
             Geo.Cells(numNode).T(end+1, :) = newTet';
             if ~isempty(Geo.Cells(numNode).AliveStatus) && exist('Set', 'var')
-                Geo.Cells(numNode).Y(end+1, :) = ComputeY(vertcat(Geo.Cells(newTet).X), Geo.Cells(numNode).X, length([Geo.Cells(newTet).AliveStatus]), Set);
-                Geo.numY = Geo.numY + 1;
-                
                 if isequal(Set.InputGeo, 'Voronoi')
+                    Geo.Cells(numNode).Y(end+1, :) = ComputeY(vertcat(Geo.Cells(newTet).X), Geo.Cells(numNode).X, 0, Set);
                     if sum(ismember(Geo.Cells(numNode).T, Geo.XgTop)) > 0
                         Geo.Cells(numNode).Y(end, 3) = Geo.Cells(numNode).Y(end, 3) / (sum(ismember(newTet, Geo.XgTop))/2);
                     elseif sum(ismember(Geo.Cells(numNode).T, Geo.XgBottom)) > 0
                         Geo.Cells(numNode).Y(end, 3) = Geo.Cells(numNode).Y(end, 3) / (sum(ismember(newTet, Geo.XgBottom))/2);
                     end
+                else
+                    Geo.Cells(numNode).Y(end+1, :) = ComputeY(vertcat(Geo.Cells(newTet).X), Geo.Cells(numNode).X, length([Geo.Cells(newTet).AliveStatus]), Set);
                 end
+                
+                Geo.numY = Geo.numY + 1;
             end
         end
     end

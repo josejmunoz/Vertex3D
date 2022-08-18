94b1bd0c7f573f312602192c4373e3c74c916ba4
Pablo Vicente Munuera <pablovm1990@gmail.com>
2022-08-18 11:16:21 +0100

    Bugfix

--- Src/Build/AddTetrahedra.m
+++ Src/Build/AddTetrahedra.m
@@ -8,19 +8,22 @@ for newTet = newTets'
             if ~isempty(Geo.Cells(numNode).AliveStatus) && exist('Set', 'var')
                 if isequal(Set.InputGeo, 'Voronoi')
                     Geo.Cells(numNode).Y(end+1, :) = ComputeY(vertcat(Geo.Cells(newTet).X), Geo.Cells(numNode).X, 0, Set);
-                    if sum(ismember(Geo.Cells(numNode).T, Geo.XgTop)) > 0
+                    if sum(ismember(Geo.Cells(numNode).T(end, :), Geo.XgTop)) > 0
                         Geo.Cells(numNode).Y(end, 3) = Geo.Cells(numNode).Y(end, 3) / (sum(ismember(newTet, Geo.XgTop))/2);
-                    elseif sum(ismember(Geo.Cells(numNode).T, Geo.XgBottom)) > 0
+                    elseif sum(ismember(Geo.Cells(numNode).T(end, :), Geo.XgBottom)) > 0
                         Geo.Cells(numNode).Y(end, 3) = Geo.Cells(numNode).Y(end, 3) / (sum(ismember(newTet, Geo.XgBottom))/2);
                     end
                 else
                     Geo.Cells(numNode).Y(end+1, :) = ComputeY(vertcat(Geo.Cells(newTet).X), Geo.Cells(numNode).X, length([Geo.Cells(newTet).AliveStatus]), Set);
                 end
                 
+                if any(isinf(Geo.Cells(numNode).Y(end, :)))
+                    disp('mierda');
+                end
                 Geo.numY = Geo.numY + 1;
             end
         end
-    end
+    end   
 end
 end
 

06d672791e41c359888a123fc5b5b561f02de65f
Pablo Vicente Munuera <pablovm1990@gmail.com>
2022-08-23 14:03:13 +0100

    Update Bubbles Remodelling

--- Src/Input/Remodelling_Bubbles.m
+++ Src/Input/Remodelling_Bubbles.m
@@ -1,7 +1,7 @@
 Set.InputGeo = 'Bubbles';
 
-Geo.nx = 3; 
-Geo.ny = 1; 
+Geo.nx = 1; 
+Geo.ny = 3; 
 Geo.nz = 1;
 
 %Set.TotalCells = 40;
@@ -9,30 +9,30 @@ Geo.nz = 1;
 Set.Ablation = 0;
 Geo.cellsToAblate = [5];
  
-Set.lambdaV = 10; 
+Set.lambdaV = 20; 
  
 Set.tend=200; 
-Set.Nincr=800; 
+Set.Nincr=Set.tend*40; 
  
 Set.Substrate = 0;
 
-Set.Contractility = true;
-Set.cLineTension = 0.001;
+Set.Contractility = false;
+Set.cLineTension = 0;
 
 Set.Remodelling = 1;
 Set.RemodelingFrequency = 0.05;
-Set.lambdaB = 0.0000001;
-Set.RemodelTol = 6e-08;
+Set.lambdaB = 0.001;
+Set.RemodelTol = Set.lambdaB*0.5;
 
 Set.BC = 2;
-Set.TStartBC = Set.tend;
-Set.TStopBC = Set.tend;
+Set.TStartBC = 50;
+Set.TStopBC = 10;
 Set.dx = 1; % compression only (2 for stretching)
 Set.VPrescribed = realmax;
 Set.VFixd = -1;
 
-Set.lambdaS1 = 0.1;
-Set.lambdaS2 = 0.01; % compression only. 0.8 for stretch
+Set.lambdaS1 = 1;
+Set.lambdaS2 = 0.1; % compression only. 0.8 for stretch
 Set.ApplyBC=true;
  
-Set.OutputFolder='Result/Remodelling';
+Set.OutputFolder='Result/Remodelling_Bubbles';

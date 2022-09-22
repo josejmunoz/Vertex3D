deb37838de496bf884c5b354f237acae20f37fe1
Pablo <pablovm1990@gmail.com>
2022-09-22 11:05:19 +0100

    Connecting tetrahedra for a T1

--- Src/Build/ConnectTetrahedra.m
+++ Src/Build/ConnectTetrahedra.m
@@ -3,44 +3,40 @@ function [Tnew, Ynew] = ConnectTetrahedra(Geo, nodeToRemove, nodesToChange, oldT
 %   Detailed explanation goes here
 
 Tnew = [];
+allTs = vertcat(Geo.Cells.T);
 if isequal(Set.InputGeo, 'Voronoi')
-    [closestDistance, closestID] = pdist2(vertcat(Geo.Cells(nodesToChange).X), Geo.Cells(nodeToRemove).X, 'euclidean', 'Smallest', 1);
-    nodesToCombine = [nodesToChange(closestID), nodeToRemove];
-    oldYs = cellfun(@(x) GetYFromTet(Geo, x), num2cell(oldTets, 2), 'UniformOutput', false);
-    oldYs = vertcat(oldYs{:});
+    if length(cellNodeLoosing) == 1
+        nodesLoosing = [cellNodeLoosing, mainNodes(vertcat(Geo.Cells(mainNodes).AliveStatus) == 0)];
+    end
+    
+    mainNodesToConnect = setdiff(mainNodes, nodesLoosing);
+    
     if length(mainNodes) >= 4
-        tetsToChange = oldTets(sum(ismember(oldTets, [nodesToChange(closestID) nodeToRemove]), 2) > 1, :);
-        Ynew = oldYs(sum(ismember(oldTets, [nodesToChange(closestID) nodeToRemove]), 2) > 1, :);
-        mainNodes(ismember(mainNodes, cellNodeLoosing)) = [];
-        mainNodes(vertcat(Geo.Cells(mainNodes).AliveStatus) == 0) = [];
-        if size(tetsToChange, 1) == 2
-            tetXs = zeros(size(tetsToChange, 1), 3);
-            for numTet = 1:size(tetsToChange, 1)
-                tetXs(numTet, :) = mean(vertcat(Geo.Cells(tetsToChange(numTet, :)).X));
-            end
-            
-            for nodeToConnect = mainNodes'
-                nodeX = vertcat(Geo.Cells(nodeToConnect).X);
-                [closestDistance, closestID] = pdist2(tetXs(:, 1:2), nodeX(1:2), 'euclidean', 'Smallest', 1);
-                
-                newTet = tetsToChange(closestID, :);
-                newTet(newTet == cellNodeLoosing) = nodeToConnect;
-                Tnew = vertcat(Tnew, newTet);
-                tetsToChange(closestID, :) = [];
-                tetXs(closestID, :) = [];
-            end
-        else
-            error('WARNINGGGG check connect tetrahedra!!!!')
+        nodesToConnect = unique([unique(allTs(sum(ismember(allTs, nodesLoosing), 2)> 1, :)); nodesToChange]);
+        
+        nodesConnectedToMainNodes = unique([getNodeNeighbours(Geo, mainNodesToConnect(1)); getNodeNeighbours(Geo, mainNodesToConnect(2))]);
+        nodesConnectedToLoosingNodes = intersect(nodesToConnect, getNodeNeighbours(Geo, cellNodeLoosing));
+        
+        newCellBoundaryNode = setdiff(nodesConnectedToLoosingNodes, nodesConnectedToMainNodes);
+        newCellBoundaryNode = newCellBoundaryNode(ismember(newCellBoundaryNode, Geo.XgID));
+        
+        if length(newCellBoundaryNode) > 1
+            error('Need to check this!')
         end
+        
+        newCellBoundaryNode_Neighbours = getNodeNeighbours(Geo, newCellBoundaryNode);
+        opposedNodesToConnect = setdiff(intersect(nodesConnectedToLoosingNodes, newCellBoundaryNode_Neighbours), [nodeToRemove, newCellBoundaryNode]);
+        
+        Tnew = [newCellBoundaryNode, nodeToRemove, mainNodesToConnect(1), intersect(getNodeNeighbours(Geo, mainNodesToConnect(1)), opposedNodesToConnect); ...
+            newCellBoundaryNode, nodeToRemove, mainNodesToConnect(2), intersect(getNodeNeighbours(Geo, mainNodesToConnect(2)), opposedNodesToConnect)];
+        
     else %% 3 mainNodes ('common')
         [~, Tnew, Ynew, removedTets, replacedTets] = CombineTwoGhostNodes(Geo, Set, nodesToCombine, oldTets, oldYs);
     end
-    
-    Tnew
 else
     
     if length(nodesToChange) > 4
-        Tnew = nodesToChange(delaunayn(vertcat(Geo.Cells(nodesToChange).X), {'Qv', 'Q7'}));
+        Tnew = nodesToConnect(delaunayn(vertcat(Geo.Cells(nodesToConnect).X), {'Qv', 'Q7'}));
     else
         Tnew = nodesToChange';
     end

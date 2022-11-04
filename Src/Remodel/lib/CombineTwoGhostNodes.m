b0a0aba88be405ce611935bb8977360476334421
Pablo <pablovm1990@gmail.com>
2022-11-04 10:01:27 +0000

    cleaning up

--- Src/Remodel/lib/CombineTwoGhostNodes.m
+++ Src/Remodel/lib/CombineTwoGhostNodes.m
@@ -1,17 +1,16 @@
 function [Geo, Tnew, Ynew, removedTets, replacedTets] = CombineTwoGhostNodes(Geo, Set, nodesToCombine, oldTets, oldYs)
 %COMBINETWOGHOSTNODES Summary of this function goes here
 %   Detailed explanation goes here
-
     Ynew = [];
     Tnew = [];
     if isempty([Geo.Cells(nodesToCombine).AliveStatus]) %% All of them need to be ghost nodes
         CellsToCombine = [Geo.Cells(nodesToCombine)];
-        
+
         newCell = CellsToCombine(1);
-        
+
         newCell.X = mean(vertcat(CellsToCombine.X));
         newCell.T = vertcat(CellsToCombine.T);
-        
+
         %Replace old for new ID
         replacingTets = ismember(newCell.T, CellsToCombine(2).ID);
         replacedTets = newCell.T(any(replacingTets, 2), :);
@@ -22,22 +21,15 @@ function [Geo, Tnew, Ynew, removedTets, replacedTets] = CombineTwoGhostNodes(Geo
         newCell.T = newCell.T(nonRepeatIDs, :);
         % Removing Tets with the new cell twice or more within the Tet
         newCell.T(sum(ismember(newCell.T, nodesToCombine(1)), 2) > 1, :) = [];
-        
+
         for numCell = [Geo.Cells.ID]
             currentTets = Geo.Cells(numCell).T;
-            if any(any(ismember(currentTets, nodesToCombine(2))))
+            if any(any(ismember(currentTets, [nodesToCombine(2) nodesToCombine(1)])))
                 %% Replace 'old' cell by 'new' cell
                 replacingTets = ismember(currentTets, nodesToCombine(2));
                 Geo.Cells(numCell).T(replacingTets) = nodesToCombine(1);
                 currentTets = Geo.Cells(numCell).T;
-                
-                % Recalculate Ys
-%                 if ~isempty(Geo.Cells(numCell).Y)
-%                     for idTet = find(any(replacingTets, 2))'
-%                         Geo.Cells(numCell).Y(idTet, :) = ComputeY(vertcat(Geo.Cells(currentTets(idTet, :)).X), newCell.X, length([Geo.Cells(currentTets(idTet, :)).AliveStatus]), Set);
-%                     end
-%                 end
-                
+
                 %% Remove repeated Tets
                 % IDs are not ordered in the same way for different cells
                 % but same Tet
@@ -45,26 +37,26 @@ function [Geo, Tnew, Ynew, removedTets, replacedTets] = CombineTwoGhostNodes(Geo
                 checkRepatedCells = sum(ismember(currentTets, nodesToCombine(1)), 2) > 1;
                 Geo.Cells(numCell).T(checkRepeatedTets | checkRepatedCells, :) = [];
                 replacingTets(checkRepeatedTets | checkRepatedCells, :) = [];
-                
+
                 if ~isempty(Geo.Cells(numCell).Y)
                     Geo.Cells(numCell).Y(checkRepeatedTets | checkRepatedCells, :) = [];
                     for numTet = find(any(replacingTets, 2))'
                         Geo.Cells(numCell).Y(numTet, :) = ComputeY(Geo, currentTets(numTet, :), newCell.X, Set);
                         Ynew(end+1, :) = Geo.Cells(numCell).Y(numTet, :);
                         Tnew(end+1, :) = Geo.Cells(numCell).T(numTet, :);
-                    end         
+                    end
                 end
             end
         end
-        
+
         %Update the 'new' cell
         Geo.Cells(nodesToCombine(1)) = newCell;
         %Remove the 'old' cell
-        %Geo.Cells(nodesToCombine(2)).X = []; 
+        %Geo.Cells(nodesToCombine(2)).X = [];
         Geo.Cells(nodesToCombine(2)).T = [];
         Geo.XgID(ismember(Geo.XgID, nodesToCombine(2))) = [];
     end
-    
+
     [Tnew, newIds] = unique(sort(Tnew, 2), 'rows');
     Ynew = Ynew(newIds, :);
 end

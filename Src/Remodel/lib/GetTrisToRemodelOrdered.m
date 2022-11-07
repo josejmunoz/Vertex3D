d3154dbae6fdcd5b527f56237fac954a99814f85
Pablo <pablovm1990@gmail.com>
2022-11-07 13:35:41 +0000

    Refactoring for T1 transition

--- Src/Remodel/lib/GetTrisToRemodelOrdered.m
+++ Src/Remodel/lib/GetTrisToRemodelOrdered.m
@@ -19,6 +19,7 @@ ghostPairs = unique(sort(ghostPairs, 2), 'rows');
 
 segmentFeatures = table();
 for ghostPair = ghostPairs'
+    ghostPair = ghostPair';
     % Edge length
     x1 = Geo.Cells(ghostPair(1)).X;
     x2 = Geo.Cells(ghostPair(2)).X;
@@ -86,68 +87,38 @@ for ghostPair = ghostPairs'
         
         % Change the ghostPair to intercalate
         neighboursToT1 = neighboursToT1([Geo.Cells(neighboursToT1).AliveStatus] > 0);
-        ghostPair = [ghostNodesToT1 neighboursToT1(connectedNodes)];
+        if sum(connectedNodes) > 1
+            %% T1 transition
+            ghostPair = [repmat(ghostNodesToT1, sum(connectedNodes), 1) neighboursToT1(connectedNodes)];
+        else
+            %% T1 on edge
+            ghostPair = [ghostNodesToT1 neighboursToT1(connectedNodes)];
+        end
     else
         if edgeLength > Set.RemodelTol
             continue
         end
     end
     
-    % Edge valence (number of shared tets)
-    [valence, sharedTets] = edgeValence(Geo, ghostPair);
-    
-    % Face IDs involved
-    faceIDs = [];
-    for cellNode = sharedCellNodes'
-        for numFace = 1:length(Geo.Cells(cellNode).Faces)
-            face = Geo.Cells(cellNode).Faces(numFace);
-            if any(ismember(ghostPair, face.ij))
-                faceIDs(end+1) = face.globalIds;
+    for gPair = ghostPair'
+        % Edge valence (number of shared tets)
+        [valence, sharedTets] = edgeValence(Geo, gPair);
+
+        % Face IDs involved
+        faceIDs = [];
+        for cellNode = sharedCellNodes'
+            for numFace = 1:length(Geo.Cells(cellNode).Faces)
+                face = Geo.Cells(cellNode).Faces(numFace);
+                if any(ismember(gPair, face.ij))
+                    faceIDs(end+1) = face.globalIds;
+                end
             end
         end
+
+        % Add it to the table
+        segmentFeatures(end+1, :) = table(gPair(1), gPair(2), edgeLength, valence, {sharedCellNodes}, {faceIDs}, {neighbours_1}, {neighbours_2}, {sharedTets});
     end
-    
-    % Add it to the table
-    segmentFeatures(end+1, :) = table(ghostPair(1), ghostPair(2), edgeLength, valence, {sharedCellNodes}, {faceIDs}, {neighbours_1}, {neighbours_2}, {sharedTets});
 end
-
-% allTs = vertcat(Geo.Cells.T);
-% cellNodes = 1:Geo.nCells;
-% cellNodes = cellNodes(~cellfun(@isempty, {Geo.Cells(1:Geo.nCells).AliveStatus}));
-% aliveCellNodes = cellNodes([Geo.Cells(cellNodes).AliveStatus] == 1);
-% cellTets = allTs(sum(ismember(allTs, cellNodes), 2) == 1, :);
-% triMesh = cellTets(sum(ismember(cellTets, aliveCellNodes), 2) == 1, :);
-% triMesh = unique(sort(triMesh, 2), 'rows');
-% 
-% for numGhostNode = Geo.XgID
-%     aspectRatio = [];
-%     for tet = triMesh(any(ismember(triMesh, numGhostNode), 2), :)'
-%         ghostNodes = tet(ismember(tet, Geo.XgID));
-%         cellNode = tet(~ismember(tet, Geo.XgID));
-%         x1 = Geo.Cells(ghostNodes(1)).X;
-%         x2 = Geo.Cells(ghostNodes(2)).X;
-%         x3 = Geo.Cells(ghostNodes(3)).X;
-%         sideLengths(1) = norm(x1 - x2);
-%         sideLengths(2) = norm(x2 - x3);
-%         sideLengths(3) = norm(x3 - x1);
-%         [aspectRatio(end+1)] = ComputeTriAspectRatio(sideLengths);
-%     end
-%     if ~isempty(aspectRatio)
-%         energyPerCellAndFaces(end+1, 1:2) = horzcat(numGhostNode, mean(aspectRatio));
-%     end
-% end
-% energyPerCellAndFaces
-
-% for c = 1:Geo.nCells
-%     for numFace = 1:length(Geo.Cells(c).Faces)
-%         face = Geo.Cells(c).Faces(numFace);
-%         
-%         if Geo.Cells(c).AliveStatus == 1 && median([face.Tris.AspectRatio]) >= Set.RemodelTol && ~isequal(face.InterfaceType, 'CellCell') 
-%             energyPerCellAndFaces(end+1, 1:6) = horzcat(c, numFace, max([face.Tris.AspectRatio]), face.globalIds, face.ij);
-%         end
-%     end
-% end
-% 
 
 if ~isempty(segmentFeatures)
     [segmentFeatures] = sortrows(segmentFeatures, 3, 'ascend');

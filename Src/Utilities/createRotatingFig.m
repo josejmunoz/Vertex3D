86ca5ae4d15fe60fe22d199e65bb5e4f783e7d32
Pablo Vicente Munuera <pablovm1990@gmail.com>
2022-11-11 14:41:06 +0000

    comments

--- Src/Utilities/createRotatingFig.m
+++ Src/Utilities/createRotatingFig.m
@@ -1,6 +1,14 @@
 function [] = createRotatingFig(fileName)
 %CREATEROTATINGFIG Summary of this function goes here
 %   Detailed explanation goes here
+%
+%   You can use it like:
+%   files = dir('/Users/pablovm/Dropbox (UCL)/TetsToShowIntercalation/*.fig')
+%   for numFile = 1:length(files)
+%       uiopen(fullfile(files(numFile).folder, files(numFile).name), 1)
+%       createRotatingFig(fullfile(files(numFile).folder, files(numFile).name))
+%   end
+
 v = VideoWriter(fileName, 'MPEG-4'); %% if linux use other than mp4
 v.Quality = 30;
 open(v);

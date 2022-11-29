1b4fc314313f07f388d3d76458aed1e14e1f2a79
Pablo <pablovm1990@gmail.com>
2022-11-29 17:14:07 +0000

    Vol0 should be the same between cells

--- Src/Geo/InitializeGeometry_3DVoronoi.m
+++ Src/Geo/InitializeGeometry_3DVoronoi.m
@@ -162,6 +162,8 @@ for c = 1:Geo.nCells
             end
         end
     end
+    
+    Geo.Cells(c).Vol0 = mean([Geo.Cells(1:Geo.nCells).Vol]);
 end
 Geo.AvgEdgeLength_Top = mean(edgeLengths_Top);
 Geo.AvgEdgeLength_Bottom = mean(edgeLengths_Bottom);
@@ -171,7 +173,7 @@ Set.BarrierTri0=Set.BarrierTri0/10;
 Geo.RemovedDebrisCells = [];
 
 minZs = min(vertcat(Geo.Cells(1:Geo.nCells).Y));
-Geo.CellHeightOriginal = abs(minZs(3) * 2);
+Geo.CellHeightOriginal = abs(minZs(3)) * 2;
 
 end
 

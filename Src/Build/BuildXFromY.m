50c6d75ea48deb8d3a43bc5dce245537a20cea39
Pablo <pablovm1990@gmail.com>
2023-01-30 10:39:57 +0000

    Fix and optimising buildXFromY

--- Src/Build/BuildXFromY.m
+++ Src/Build/BuildXFromY.m
@@ -1,24 +1,27 @@
 function Geo = BuildXFromY(Geo_n, Geo)
-    for c = 1:length(Geo.Cells)
+
+    proportionOfMax = 0;
+
+    aliveCells = [Geo.Cells(~cellfun(@isempty, {Geo.Cells.AliveStatus})).ID];
+    allCellsToUpdate = setdiff(1:length(Geo.Cells), vertcat(Geo.BorderCells, Geo.BorderGhostNodes));
+    for c = allCellsToUpdate
 		% TODO FIXME, seems not optimal.. 2 loops necessary ?
         if ~isempty(Geo.Cells(c).T)
             if ismember(c, Geo.XgID) % Updating Ghost node
                 dY = zeros(size(Geo.Cells(c).T,1), 3);
                 for tet = 1:size(Geo.Cells(c).T,1)
                     gTet = Geo.Cells(c).T(tet,:);
-                    for cm = [Geo.Cells(~cellfun(@isempty, {Geo.Cells.AliveStatus})).ID]
-                        Cell   = Geo.Cells(cm);
-                        Cell_n = Geo_n.Cells(cm);
-                        hit = sum(ismember(Cell.T,gTet),2)==4;
-                        if any(hit)
-                            dY(tet,:) = Cell.Y(hit,:) - Cell_n.Y(hit,:);
-                        end
-                    end
+                    gTet_Cells = gTet(ismember(gTet, aliveCells));
+                    cm = gTet_Cells(1);
+                    Cell   = Geo.Cells(cm);
+                    Cell_n = Geo_n.Cells(cm);
+                    hit = sum(ismember(Cell.T,gTet),2)==4;
+                    dY(tet,:) = Cell.Y(hit,:) - Cell_n.Y(hit,:);
                 end
-                Geo.Cells(c).X = Geo.Cells(c).X + mean(dY);
+                Geo.Cells(c).X = Geo.Cells(c).X + (proportionOfMax) * max(dY) + (1 - proportionOfMax) * mean(dY);
             else % Updating a main node
                 dY = Geo.Cells(c).Y - Geo_n.Cells(c).Y;
-                Geo.Cells(c).X = Geo.Cells(c).X + mean(dY);
+                Geo.Cells(c).X = Geo.Cells(c).X + (proportionOfMax) * max(dY) + (1 - proportionOfMax) * mean(dY);
             end
         end
     end

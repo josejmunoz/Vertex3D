8ece5afee91766698b05659b17f8721f3636f48c
Pablo <pablovm1990@gmail.com>
2023-02-17 16:40:13 +0000

    bugfix

--- Src/Remodel/Flip7N.m
+++ Src/Remodel/Flip7N.m
@@ -6,6 +6,6 @@ flipName = '7-N';
 [Ynew, Tnew] = YFlip7N(oldTets, segmentToChange, Geo, Set);
 
 if ~isempty(Tnew)
-    [Geo_0, Geo_n, Geo, Dofs, newYgIds, hasConverged] = PostFlip(Tnew, Ynew, oldTets, Geo, Geo_n, Geo_0, Dofs, newYgIds, Set, flipName);
+    [Geo_0, Geo_n, Geo, Dofs, newYgIds, hasConverged] = PostFlip(Tnew, Ynew, oldTets, Geo, Geo_n, Geo_0, Dofs, newYgIds, Set, flipName, segmentToChange);
 end
 end

486c7edc318f22ad9837b0a2ee1605f6131d5fc4
Pablo Vicente Munuera <pablovm1990@gmail.com>
2023-04-04 17:13:06 +0100

    visualize tets changed parameters

--- Src/Utilities/visualizeTets.m
+++ Src/Utilities/visualizeTets.m
@@ -1,8 +1,8 @@
-function visualizeTets(tets, Geo)
+function visualizeTets(tets, X)
 %VISUALIZETETS Summary of this function goes here
 %   Detailed explanation goes here
-allNodes = vertcat(Geo.Cells.X);
-figure, tetramesh(tets, vertcat(Geo.Cells.X))
+allNodes = X;
+figure, tetramesh(tets, X)
 uniqueNodes = unique(tets);
 text(allNodes(uniqueNodes, 1), allNodes(uniqueNodes, 2), allNodes(uniqueNodes, 3), ...
     cellfun(@num2str, num2cell(uniqueNodes), 'UniformOutput', false),'VerticalAlignment','bottom','HorizontalAlignment','right')

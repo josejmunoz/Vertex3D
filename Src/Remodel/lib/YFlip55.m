9f489f5aa10b1a87e0104f91fb3802cca5c1932e
Pablo Vicente Munuera <pablovm1990@gmail.com>
2023-04-17 13:51:19 +0100

    Towards general flip

--- Src/Remodel/lib/YFlip55.m
+++ Src/Remodel/lib/YFlip55.m
@@ -3,7 +3,7 @@ function [Ynew, Tnew] = YFlip55(oldTets, oldYs, XsToDisconnect, Geo, Set)
 %   Detailed explanation goes here
 
 Ynew = [];
-
+allXs = vertcat(Geo.Cells.X);
 ghostNodesWithoutDebris = setdiff(Geo.XgID, Geo.RemovedDebrisCells);
 
 Xs = unique(oldTets);
@@ -27,24 +27,40 @@ visualizeTets(oldTets, vertcat(Geo.Cells.X))
 % Or remo the edges that pass through inside the tetrahedro
 boundaryNodes = unique(oldTets); % All the nodes should be boundary nodes
 tris = triangulation(oldTets, vertcat(Geo.Cells.X));
-tris.edges
+trisEdges = tris.edges;
 
-% nodes should be connected with two of the same layer and 1 different in
-% the boundary.
-
-boundaryEdges = [];
+% nodes should be connected with two of the same layer.
+tets_Ghost = tris.ConnectivityList(sum(ismember(tris.ConnectivityList, Geo.XgID), 2)>2, :);
+tets_Ghost = sort(tets_Ghost, 2);
+tris_Ghost = tets_Ghost(:, 2:end);
+if any(~ismember(tris_Ghost(:), Geo.XgID))
+    error('Yflip55')
+end
+tets_Cells = tris.ConnectivityList(sum(~ismember(tris.ConnectivityList, Geo.XgID), 2)>2, :);
+tets_Cells = sort(tets_Cells, 2);
+tris_Cells = tets_Cells(:, 1:3);
+if any(ismember(tris_Cells(:), Geo.XgID))
+    error('Yflip55')
+end
+[nodesExt_Cells, boundary_Ghost] = GetBoundary2D(tris_Ghost, allXs);
+[nodesExt_Ghost, boundary_Cells] = GetBoundary2D(tris_Cells, allXs);
+boundaryEdges = vertcat(sort(boundary_Ghost, 2), sort(boundary_Cells, 2));
 
 %% Step 2: Get edges that can be added when removing the other one
 % Find the edges that may be connected and are not connected
 possibleEdges = nchoosek(boundaryNodes, 2);
-possibleEdges = setdiff(possibleEdges, boundaryEdges);
-possibleEdges = setdiff(possibleEdges, XsToDisconnect);
+possibleEdges(ismember(possibleEdges, boundaryEdges, 'rows'), :) = [];
+possibleEdges(ismember(possibleEdges, XsToDisconnect, 'rows'), :) = [];
 
 %Remove other impossible edges
 
 %% Step 3: Select the edge to add
 % Aim: connecting the XsToDisconnect_c to another gNode
 % Based on Valence? distance? who should be intercalating with?
+
+finalEdges = boundaryEdges;
+
+possibleEdges
 
 %% Step 4: Propagate the change to get the remaining tets
 % Create tetrahedra
@@ -130,10 +146,11 @@ else
 end
 end
 
-function nodesExt=GetBoundary3D(T,X)
+function [nodesExt, pairsExt]=GetBoundary2D(T,X)
 np=size(X,1);
 nele=size(T,1);
 nodesExt=zeros(1,np);
+pairsExt=[];
 for e=1:nele
     Te=[T(e,:) T(e,1)];
     Sides=[0 0 0];
@@ -147,6 +164,7 @@ for e=1:nele
         end
         if Sides(s)==0
             nodesExt(Te(s:s+1))=Te(s:s+1);
+            pairsExt(end+1, 1:2) = Te(s:s+1);
         end
     end
 end

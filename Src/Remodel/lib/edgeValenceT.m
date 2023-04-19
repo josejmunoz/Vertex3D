a78b2bd0f6805b8f79f1dffd6be8b115a4b8e3fa
Pablo Vicente Munuera <pablovm1990@gmail.com>
2023-04-19 10:48:10 +0100

    returning tetIDs

--- Src/Remodel/lib/edgeValenceT.m
+++ Src/Remodel/lib/edgeValenceT.m
@@ -1,4 +1,4 @@
-function [valence, sharedTets] = edgeValenceT(tets, nodesEdge)
+function [valence, sharedTets, tetIds] = edgeValenceT(tets, nodesEdge)
 %EDGEVALENCE Tets in common with an edge
 %   Detailed explanation goes here
 tets1 = tets(any(ismember(tets, nodesEdge(1)), 2), :);
@@ -10,5 +10,7 @@ nodeTets2 = sort(tets2, 2);
 tetIds = ismember(nodeTets1, nodeTets2, 'rows');
 sharedTets = nodeTets1(tetIds, :);
 valence = size(sharedTets, 1);
+
+tetIds = find(ismember(sort(tets, 2), sharedTets, 'rows'));
 end
 

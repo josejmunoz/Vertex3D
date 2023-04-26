f5a89bea62cfdbe8e8c0fb938b2e4d82aa28c018
Pablo Vicente Munuera <pablovm1990@gmail.com>
2023-04-26 10:25:21 +0100

    Postflip refactor

--- Src/Remodel/lib/PostFlip.m
+++ Src/Remodel/lib/PostFlip.m
@@ -7,48 +7,22 @@ Geo_backup = Geo; Geo_n_backup = Geo_n; Geo_0_backup = Geo_0; Dofs_backup = Dofs
 
 Geo.log = sprintf('%s =>> %s-Flip: %i %i.\n', Geo.log, flipName, segmentToChange(1), segmentToChange(2));
 
-[Geo] = RemoveTetrahedra(Geo, oldTets);
-[Geo] = AddTetrahedra(Geo, Tnew, Ynew, Set);
-[Geo_n] = RemoveTetrahedra(Geo_n, oldTets);
-[Geo_n] = AddTetrahedra(Geo_n, Tnew, Ynew, Set);
-[Geo_0] = RemoveTetrahedra(Geo_0, oldTets);
-[Geo_0] = AddTetrahedra(Geo_0, Tnew, Ynew, Set);
-
-%try
-    Geo   = Rebuild(Geo, Set);
-    Geo   = BuildGlobalIds(Geo);
-    Geo   = UpdateMeasures(Geo);
-    
-    Geo_n = Rebuild(Geo_n, Set);
-    Geo_n = BuildGlobalIds(Geo_n);
-    
-    Geo_0 = Rebuild(Geo_0, Set);
-    Geo_0 = BuildGlobalIds(Geo_0);
+[Geo] = AddAndRebuildCells(Geo, oldTets, newTets, Ynew, Set, 1);
+[Geo_0] = AddAndRebuildCells(Geo_0, oldTets, newTets, Ynew, Set, 0);
+[Geo_n] = AddAndRebuildCells(Geo_n, oldTets, newTets, Ynew, Set, 0);
         
-    %% Update Geo_0 to be reset the vertices that we have changed averaging with previous Geo_0 and current Geo
-    percentageGeo = 1 - Set.Reset_PercentageGeo0;
-    for c=1:Geo.nCells
-        if ismember(c, Tnew) && ~isempty(Geo.Cells(c).AliveStatus) && Geo.Cells(c).AliveStatus == 1
-            Geo_0.Cells(c).X = Set.Reset_PercentageGeo0 * Geo_0.Cells(c).X + percentageGeo * Geo.Cells(c).X;
-            Geo_0.Cells(c).Y = Set.Reset_PercentageGeo0 * Geo_0.Cells(c).Y + percentageGeo * Geo.Cells(c).Y;
-            
-            for f=1:length(Geo.Cells(c).Faces)
-                Geo_0.Cells(c).Faces(f).Centre = Set.Reset_PercentageGeo0 * Geo_0.Cells(c).Faces(f).Centre + percentageGeo * Geo.Cells(c).Faces(f).Centre;
-            end
+%% Update Geo_0 to be reset the vertices that we have changed averaging with previous Geo_0 and current Geo
+percentageGeo = 1 - Set.Reset_PercentageGeo0;
+for c=1:Geo.nCells
+    if ismember(c, Tnew) && ~isempty(Geo.Cells(c).AliveStatus) && Geo.Cells(c).AliveStatus == 1
+        Geo_0.Cells(c).X = Set.Reset_PercentageGeo0 * Geo_0.Cells(c).X + percentageGeo * Geo.Cells(c).X;
+        Geo_0.Cells(c).Y = Set.Reset_PercentageGeo0 * Geo_0.Cells(c).Y + percentageGeo * Geo.Cells(c).Y;
+
+        for f=1:length(Geo.Cells(c).Faces)
+            Geo_0.Cells(c).Faces(f).Centre = Set.Reset_PercentageGeo0 * Geo_0.Cells(c).Faces(f).Centre + percentageGeo * Geo.Cells(c).Faces(f).Centre;
         end
     end
-    
-% catch MException
-%     Geo   = Geo_backup;
-%     Geo_n = Geo_n_backup;
-%     Geo_0 = Geo_0_backup;
-%     Geo.log = strcat(Geo.log, sprintf('=>> %s-Flip rejected: ', flipName);
-%     fprintf(MException.identifier);
-%     fprintf('\n');
-%     fprintf(MException.message);
-%     fprintf('\n');
-%     return
-% end
+end
 
 if CheckTris(Geo) %%&& ~CheckConvexity(Tnew,Geo_backup)
     PostProcessingVTK(Geo, Geo_0, Set, Set.iIncr+1)

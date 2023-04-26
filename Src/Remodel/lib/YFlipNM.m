368d3346ffce67340c11bacb030e820662c9f69f
Pablo Vicente Munuera <pablovm1990@gmail.com>
2023-04-26 10:28:44 +0100

    Cleaning and working

--- Src/Remodel/lib/YFlipNM.m
+++ Src/Remodel/lib/YFlipNM.m
@@ -2,7 +2,6 @@ function [Ynew, Tnew] = YFlipNM(oldTets, cellToIntercalateWith, oldYs, XsToDisco
 %YFLIP6N Summary of this function goes here
 %   Detailed explanation goes here
 
-allXs = vertcat(Geo.Cells.X);
 Xs_gToDisconnect = XsToDisconnect(ismember(XsToDisconnect, Geo.XgID));
 
 % Temporary remove 4-cell tetrahedra
@@ -21,31 +20,10 @@ end
 %% Step 1: Keep the boundary of tets not changed.
 % Or remo the edges that pass through inside the tetrahedro
 boundaryNodes = unique(oldTets); % All the nodes should be boundary nodes
-tris = triangulation(oldTets, allXs);
-
-% nodes should be connected with two of the same layer.
-tets_Ghost = tris.ConnectivityList(sum(ismember(tris.ConnectivityList, Geo.XgID), 2)>2, :);
-tets_Ghost = sort(tets_Ghost, 2);
-tris_Ghost = tets_Ghost(:, 2:end);
-if any(~ismember(tris_Ghost(:), Geo.XgID))
-    error('Yflip55')
-end
-tets_Cells = tris.ConnectivityList(sum(~ismember(tris.ConnectivityList, Geo.XgID), 2)>2, :);
-tets_Cells = sort(tets_Cells, 2);
-tris_Cells = tets_Cells(:, 1:3);
-if any(ismember(tris_Cells(:), Geo.XgID))
-    error('Yflip55')
-end
-[nodesExt_Cells, boundary_Ghost] = GetBoundary2D(tris_Ghost, allXs);
-[nodesExt_Ghost, boundary_Cells] = GetBoundary2D(tris_Cells, allXs);
-boundaryEdges = vertcat(sort(boundary_Ghost, 2), sort(boundary_Cells, 2));
 
 %% Step 2: Get edges that can be added when removing the other one
 % Find the edges that may be connected and are not connected
 possibleEdges = nchoosek(boundaryNodes, 2);
-% possibleEdges(ismember(possibleEdges, boundaryEdges, 'rows'), :) = [];
-% possibleEdges(ismember(possibleEdges, XsToDisconnect, 'rows'), :) = [];
-
 
 %% Step 3: Select the edge to add
 % Aim: connecting the XsToDisconnect_c to another gNode

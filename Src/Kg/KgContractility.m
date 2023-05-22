bce844ca13da67d9f9d6e2e47fc2f6dd5b895186
Pablo Vicente Munuera <pablovm1990@gmail.com>
2023-05-22 13:21:23 +0100

    Adding Face contractility between face centres and vertices

--- Src/Kg/KgContractility.m
+++ Src/Kg/KgContractility.m
@@ -28,15 +28,38 @@ function [g, K, Energy_T, Geo] = KgContractility(Geo, Set)
                 
                 for numTri = 1:length(currentFace.Tris)
                     currentTri = Geo.Cells(c).Faces(numFace).Tris(numTri);
-                    if length(currentTri.SharedByCells) > 1
-                        C = getContractilityBasedOnLocation(currentFace, currentTri, Geo, Set);
-                        
-                        y_1 = currentCell.Y(currentTri.Edge(1), :);
-                        y_2 = currentCell.Y(currentTri.Edge(2), :);
-    
+                    n3 = Geo.Cells(c).Faces(numFace).globalIds;
+                    nY_original = [Geo.Cells(c).globalIds(currentTri.Edge)', n3];
+                    if Geo.Remodelling
+                        if ~any(ismember(nY_original, Geo.AssemblegIds))
+                            continue
+                        end
+                    end
+
+                    C = getContractilityBasedOnLocation(currentFace, currentTri, Geo, Set);
+                    if length(currentTri.SharedByCells) == 1
+                        C = C * Set.cLineTensionMembrane;
+                    end
+
+                    y1 = currentCell.Y(currentTri.Edge(1), :);
+                    y2 = currentCell.Y(currentTri.Edge(2), :);
+                    y3 = Geo.Cells(c).Faces(numFace).Centre;
+
+                    ys(1, :) = {y1, y2};
+                    ys(2, :) = {y2, y3};
+                    ys(3, :) = {y3, y1};
+                    
+                    nY(1, 1:2) = nY_original([1 2]);
+                    nY(2, 1:2) = nY_original([2 3]);
+                    nY(3, 1:2) = nY_original([3 1]);
+                    
+                    for numY = 1:size(ys, 1)
+                        y_1 = ys{numY, 1}';
+                        y_2 = ys{numY, 2}';
+
                         %% Calculate residual g
                         g_current = computeGContractility(l_i0, y_1, y_2, C);
-                        ge = Assembleg(ge, g_current, currentCell.globalIds(currentTri.Edge));
+                        ge = Assembleg(ge, g_current, nY(numY, :));
     
                         %% Save contractile forces (g) to output
                         Geo.Cells(c).Faces(numFace).Tris(numTri).ContractileG = norm(g_current(1:3));
@@ -44,10 +67,10 @@ function [g, K, Energy_T, Geo] = KgContractility(Geo, Set)
                         %% Calculate Jacobian
                         K_current = computeKContractility(l_i0, y_1, y_2, C);
     
-                        K = AssembleK(K, K_current, currentCell.globalIds(currentTri.Edge));
+                        K = AssembleK(K, K_current, nY(numY, :));
     
                         %% Calculate energy
-                        Energy_c = Energy_c + computeEnergyContractility(l_i0, norm(y_1 - y_2), C);                    
+                        Energy_c = Energy_c + computeEnergyContractility(l_i0, norm(y_1 - y_2), C);
                     end
                 end
             end

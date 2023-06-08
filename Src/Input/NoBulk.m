d2a0d5f30547efc46d7c87ed525d2923dd35ef74
Pablo Vicente Munuera <pablovm1990@gmail.com>
2023-06-08 10:36:48 +0100

    Improving noBulk parameters

--- Src/Input/NoBulk.m
+++ Src/Input/NoBulk.m
@@ -6,14 +6,17 @@ Set.mu_bulk	= 0; %% 1000
 Set.lambda_bulk	= 0; %% 500
 
 Set.nu	= 5000;
+Set.Nincr = 61*10;
+
+Set.lambdaB	= 5;
+
 Set.lambdaV = 10;
 
 Set.kSubstrate = 100;
 
-Set.cLineTension = 0.05;
+Set.cLineTension = 0.5;
+Set.cLineTensionMembrane = 0;
 Set.purseStringStrength = 2.5;
-Set.RemodelStiffness = 0.65;
 
-Set.lambdaS1 = 0.5;
-Set.lambdaS2 = Set.lambdaS1 * 0.9;
-Set.lambdaS3 = Set.lambdaS1;
+Set.lambdaS1 = 3;
+Set.VTK = false;

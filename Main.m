a8e1c7d38568f6d611b3da23c9074e3a30cdb3fa
Pablo Vicente Munuera <pablovm1990@gmail.com>
2023-06-27 09:54:35 +0100

    Bugfix

--- Main.m
+++ Main.m
@@ -37,7 +37,7 @@ for numLine = 1:length(Sets)
     prevLog = '';
     tStart = tic;
     didNotConverge = false;
-%     try
+    try
         Geo = Geos{numLine};
         Set = Sets{numLine};
         Geo.log = sprintf('--------- SIMULATION STARTS ---------\n');
@@ -56,9 +56,9 @@ for numLine = 1:length(Sets)
             [Geo, Geo_n, Geo_0, Set, Dofs, EnergiesPerTimeStep, t, numStep, tr, relaxingNu, backupVars, didNotConverge] = IterateOverTime(Geo, Geo_n, Geo_0, Set, Dofs, EnergiesPerTimeStep, t, numStep, tr, relaxingNu, backupVars);
            
         end
-%     catch ME
-%         Geo.log = sprintf("%s\n ERROR: %s", Geo.log, ME.message);
-%     end
+    catch ME
+        Geo.log = sprintf("%s\n ERROR: %s", Geo.log, ME.message);
+    end
     tEnd = duration(seconds(toc(tStart)));
     tEnd.Format = 'hh:mm:ss';
     Geo.log = sprintf("%s Total real run time %s \n", Geo.log, tEnd);

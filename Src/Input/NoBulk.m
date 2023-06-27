bb931a57c88581f89ee40548bb48fcf11f10bb9e
Pablo Vicente Munuera <pablovm1990@gmail.com>
2023-06-27 09:06:58 +0100

    Working

--- Src/Input/NoBulk.m
+++ Src/Input/NoBulk.m
@@ -1,22 +1,27 @@
 Set.InputGeo = 'Voronoi';
-Geo.cellsToAblate = [1:3];
+% 40 cells; 3 cells to ablate
+% 110 cells; 5 cells to ablate
+Set.TotalCells = 110;
+Geo.cellsToAblate = [1:5];
 
 Set.InPlaneElasticity = false;
 Set.mu_bulk	= 0; %% 1000
 Set.lambda_bulk	= 0; %% 500
 
 Set.nu	= 5000;
-Set.Nincr = 61*10;
+Set.Nincr = 61*2;
 
-Set.lambdaB	= 5;
+Set.lambdaB	= 1; % Smaller number, more energy
+Set.lambdaR	= 1; 
 
 Set.lambdaV = 10;
 
-Set.kSubstrate = 100;
+Set.kSubstrate = 1000;
 
-Set.cLineTension = 0.5;
+Set.cLineTension = 0.2;
 Set.cLineTensionMembrane = 0;
-Set.purseStringStrength = 2.5;
+Set.purseStringStrength = 1.5;
 
-Set.lambdaS1 = 3;
+Set.lambdaS1 = 1;
+Set.lambdaS2 = 0.01*Set.lambdaS1;
 Set.VTK = false;

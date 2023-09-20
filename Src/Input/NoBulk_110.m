3dcb87475957b928613f5ceecb572f634582a9dd
Pablo Vicente Munuera <pablovm1990@gmail.com>
2023-09-20 09:13:34 +0100

    This set works nicely, but too fast

--- Src/Input/NoBulk_110.m
+++ Src/Input/NoBulk_110.m
@@ -1,8 +1,8 @@
-Set.InputGeo = 'Voronoi';
+Set.InputGeo = 'VertexModelTime';
 % 40 cells; 3 cells to ablate
-% 110 cells; 5 cells to ablate
+% 110 cells; 7 cells to ablate
 Set.TotalCells = 110;
-Geo.cellsToAblate = [1:5];
+Geo.cellsToAblate = 1:7;
 
 Set.InPlaneElasticity = false;
 Set.mu_bulk	= 0; %% 1000
@@ -12,15 +12,15 @@ Set.nu	= 5000;
 Set.Nincr = 61*2;
 
 Set.lambdaB	= 1; % Smaller number, more energy
-Set.lambdaR	= 0.1; 
+Set.lambdaR	= 0.3; 
 
-Set.lambdaV = 1000;
+Set.lambdaV = 10;
 
 Set.kSubstrate = 100;
 
-Set.cLineTension = 0.005;
-Set.purseStringStrength = 1;
+Set.cLineTension = 0.05;
+Set.purseStringStrength = 30;
 
-Set.lambdaS1 = 5;
-Set.lambdaS2 = 0.01*Set.lambdaS1;
-Set.VTK = false;
+Set.lambdaS1 = 20;
+Set.lambdaS3 = Set.lambdaS1/10;
+Set.VTK = true;
